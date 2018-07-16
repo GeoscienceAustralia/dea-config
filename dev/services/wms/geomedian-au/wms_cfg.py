f1e2733f54f86eea69d5eb55d764eb1cdd82aaa2
harshurampur <harshu.rampur@ga.gov.au>
2018-07-17 09:58:25 +1000

    Update wms_cfg.py

--- dev/services/wms/geomedian-au/wms_cfg.py
+++ dev/services/wms/geomedian-au/wms_cfg.py
@@ -13,7 +13,7 @@ service_cfg = {
     # Service title - appears e.g. in Terria catalog
     "title": "WMS server for Australian Geomedian NBART Datacube",
     # Service URL.  Should a fully qualified URL
-    "url": "https://geomedianau.wms.gadevs.ga//",
+    "url": "https://geomedianau.wms.gadevs.ga",
 
     # Supported co-ordinate reference systems
     "published_CRSs": {
