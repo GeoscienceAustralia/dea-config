c3ec4268fcbf561eab5f98f16face1353aea63e4
Robert Armstrong <robert.armstrong@ga.gov.au>
2018-09-04 17:37:26 +1000

    Update WOfS summary style

--- dev/services/wms/wofs/wms_cfg.py
+++ dev/services/wms/wofs/wms_cfg.py
@@ -96,6 +96,126 @@ service_cfg = {
     "access_constraints": "",
 }
 
+__ramp_values__ = [
+    {
+        "value": 0.002,
+        "color": "#000000",
+        "alpha": 0.0
+    },
+    {
+        "value": 0.005,
+        "color": "#8e0101",
+        "alpha": 0.25
+    },
+    {
+        "value": 0.01,
+        "color": "#cf2200",
+        "alpha": 0.75
+    },
+    {
+        "value": 0.02,
+        "color": "#e38400"
+    },
+    {
+        "value": 0.05,
+        "color": "#e3df00"
+    },
+    {
+        "value": 0.1,
+        "color": "#a6e300"
+    },
+    {
+        "value": 0.2,
+        "color": "#62e300"
+    },
+    {
+        "value": 0.3,
+        "color": "#00e32d"
+    },
+    {
+        "value": 0.4,
+        "color": "#00e384"
+    },
+    {
+        "value": 0.5,
+        "color": "#00e3c8"
+    },
+    {
+        "value": 0.6,
+        "color": "#00c5e3"
+    },
+    {
+        "value": 0.7,
+        "color": "#0097e3"
+    },
+    {
+        "value": 0.8,
+        "color": "#005fe3"
+    },
+    {
+        "value": 0.9,
+        "color": "#000fe3"
+    },
+    {
+        "value": 1.0,
+        "color": "#5700e3"
+    }
+]
+
+def __generate_color_ramp__():
+    def frange(start, stop, step):
+        i = start
+        while i < stop:
+            yield i
+            i += step
+    from colour import Color
+    values = []
+    ramp = []
+    alpha = []
+    low = None
+    high = None
+    for i, pair in enumerate(__ramp_values__):
+        if i == 0:
+            low = pair
+            continue
+        high = pair
+        prev = len(values)
+        alpha_low = low.get("alpha", 1.0)
+        alpha_high = high.get("alpha", 1.0)
+        values.extend(frange(low["value"], high["value"], 0.01))
+        if alpha_high != alpha_low:
+            alpha_range = frange(alpha_low, alpha_high, (alpha_high - alpha_low) / 0.01)
+        else:
+            alpha_range = [1.0 for i in range(len(values))]
+        ramp.extend(Color(low["color"]).range_to(Color(high["color"]), len(values) - prev))
+        alpha.extend(alpha_range)
+        low = pair
+    return zip(values, ramp, alpha)
+
+__wofs_summary_ramp__ = __generate_color_ramp__()
+__wofs_ramp_values__, __wofs_ramp_colors__, __wofs_ramp_alpha__ = zip(*__wofs_summary_ramp__)
+
+def __calculate_wofs_summary_ramp__(data, band, imgband):
+    from colour import Color
+    from bisect import bisect
+    from functools import partial
+    import xarray
+    import numpy as np
+    def process(values, colors, alpha, imgband, data):
+        i = bisect(values, data)
+        i = i if i < len(colors) else i - 1
+        c = colors[i]
+        a = alpha[i]
+        val = a if imgband is "alpha" else getattr(c, imgband)
+        return (val * 255.0)
+
+    p = partial(process, __wofs_ramp_values__, __wofs_ramp_colors__, __wofs_ramp_alpha__, imgband)
+
+    ramped = data.copy(deep=True)
+    ramped.values = np.vectorize(p)(data.values)
+    return ramped
+
+
 layer_cfg = [
     # Layer Config is a list of platform configs
     {
@@ -354,4 +474,68 @@ layer_cfg = [
 
         ],
     },
+    {
+    # Included as a keyword  for the layer
+    "label": "WOfS_Summary",
+    # Included as a keyword  for the layer
+    "type": "WOfS_Summary",
+    # Included as a keyword  for the layer
+    "variant": "Summary",
+    # The WMS name for the layer
+    "name": "wofs_summary",
+    # The Datacube name for the associated data product
+    "product_name": "wofs_summary",
+    # The Datacube name for the associated pixel-quality product (optional)
+    # The name of the associated Datacube pixel-quality product
+    #"pq_dataset": "wofs_albers",
+    # The name of the measurement band for the pixel-quality product
+    # (Only required if pq_dataset is set)
+    #"pq_band": "water",
+    # Min zoom factor - sets the zoom level where the cutover from indicative polygons
+    # to actual imagery occurs.
+    "min_zoom_factor": 0.0,
+    # The fill-colour of the indicative polygons when zoomed out.
+    # Triplets (rgb) or quadruplets (rgba) of integers 0-255.
+    "zoomed_out_fill_colour": [150, 180, 200, 160],
+    # Time Zone.  In hours added to UTC (maybe negative)
+    # Used for rounding off scene times to a date.
+    # 9 is good value for imagery of Australia.
+    "time_zone": 9,
+    # Extent mask function
+    # Determines what portions of dataset is potentially meaningful data.
+    "extent_mask_func": lambda data, band: (data['frequency'] != data['frequency'].attrs['nodata']),
+    # Flags listed here are ignored in GetFeatureInfo requests.
+    # (defaults to empty list)
+    "ignore_info_flags": [],
+
+    "styles": [
+        {
+            "name": "WOfS_frequency",
+            "title": " Wet and Dry Count",
+            "abstract": "WOfS summary determinig the count_wet and count_clear for WOfS product",
+            "needed_bands": ["frequency"],
+            "components": {
+                "red": {
+                    "frequency": __calculate_wofs_summary_ramp__
+                },
+                "green": {
+                    "frequency": __calculate_wofs_summary_ramp__
+                },
+                "blue": {
+                    "frequency": __calculate_wofs_summary_ramp__
+                },
+                "alpha": {
+                    "frequency": __calculate_wofs_summary_ramp__
+                }
+            },
+            "scale_range": [ 0, 255 ],
+        }
+    ],
+    # Default style (if request does not specify style)
+    # MUST be defined in the styles list above.
+
+    # (Looks like Terria assumes this is the first style in the list, but this is
+    #  not required by the standard.)
+    "default_style": "WOfS_frequency",
+}
 ]
