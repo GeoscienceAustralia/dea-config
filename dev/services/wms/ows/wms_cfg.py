74e62b67f1f49de1d688f8a93d298048ff86abd4
roarmstrong <34013278+roarmstrong@users.noreply.github.com>
2018-10-12 09:50:36 +1100

    Add max datasets for Barest Earth

--- dev/services/wms/ows/wms_cfg.py
+++ dev/services/wms/ows/wms_cfg.py
@@ -774,6 +774,7 @@ layer_cfg = [
                 # Min zoom factor - sets the zoom level where the cutover from indicative polygons
                 # to actual imagery occurs.
                 "min_zoom_factor": 50.0,
+                "max_datasets_wms": 1000,
                 # The fill-colour of the indicative polygons when zoomed out.
                 # Triplets (rgb) or quadruplets (rgba) of integers 0-255.
                 "zoomed_out_fill_colour": [150, 180, 200, 160],
@@ -927,6 +928,7 @@ layer_cfg = [
                 # Min zoom factor - sets the zoom level where the cutover from indicative polygons
                 # to actual imagery occurs.
                 "min_zoom_factor": 50.0,
+                "max_datasets_wms": 1000,
                 # The fill-colour of the indicative polygons when zoomed out.
                 # Triplets (rgb) or quadruplets (rgba) of integers 0-255.
                 "zoomed_out_fill_colour": [150, 180, 200, 160],
