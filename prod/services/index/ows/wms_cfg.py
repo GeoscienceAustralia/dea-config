c83c5f4420b982d547ccd5d4dab38cd08972664b
harshurampur <harshu.rampur@ga.gov.au>
2018-10-16 11:44:06 +1100

    Fix product_name for bare-earth

--- prod/services/index/ows/wms_cfg.py
+++ prod/services/index/ows/wms_cfg.py
@@ -770,10 +770,10 @@ layer_cfg = [
                 # The WMS name for the layer
                 "name": "ls8_barest_earth_mosaic",
                 # The Datacube name for the associated data product
-                "product_name": "ls8_bare_earth_albers",
+                "product_name": "ls8_barest_earth_albers",
                 # Min zoom factor - sets the zoom level where the cutover from indicative polygons
                 # to actual imagery occurs.
-                "min_zoom_factor": 25.0,
+                "min_zoom_factor": 35.0,
                 #"max_datasets_wms": 1000,
                 # The fill-colour of the indicative polygons when zoomed out.
                 # Triplets (rgb) or quadruplets (rgba) of integers 0-255.
