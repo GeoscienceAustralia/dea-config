f5095dede54fd5760970d42ee7a46e4f3dbbeca8
harshurampur <harshu.rampur@ga.gov.au>
2018-10-16 11:46:19 +1100

    update zoom factor to 35 in consistent with geomedian

--- prod/services/wms/ows/wms_cfg.py
+++ prod/services/wms/ows/wms_cfg.py
@@ -779,7 +779,7 @@ Mosaics are available for the following years:
                 "product_name": "ls8_barest_earth_albers",
                 # Min zoom factor - sets the zoom level where the cutover from indicative polygons
                 # to actual imagery occurs.
-                "min_zoom_factor": 25.0,
+                "min_zoom_factor": 35.0,
                 #"max_datasets_wms": 1000,
                 # The fill-colour of the indicative polygons when zoomed out.
                 # Triplets (rgb) or quadruplets (rgba) of integers 0-255.
