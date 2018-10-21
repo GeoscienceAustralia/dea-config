d30dc48c34f1a7ebe3aca747a966d2070b2d532e
David Gavin <GypsyBojangles@users.noreply.github.com>
2018-10-22 09:44:26 +1100

    Update SR charts to use same labels as grayscale bands
    Update SR charts to use same labels as grayscale bands

--- dev/terria/ls57_reflectance_chart_generator.sh
+++ dev/terria/ls57_reflectance_chart_generator.sh
@@ -9,14 +9,14 @@ tr -d '\n' > ls57_reflectance-chart.json <<EOF
 "template": "
 <table>
   <tr><td><b>Time</b></td><td>{{time}}</td></tr>
-  <tr><td><b>blue</b></td><td>{{bands.blue}}</td></tr>
-  <tr><td><b>green</b></td><td>{{bands.green}}</td></tr>
-  <tr><td><b>red</b></td><td>{{bands.red}}</td></tr>
-  <tr><td><b>nir</b></td><td>{{bands.nir}}</td></tr>
-  <tr><td><b>swir1</b></td><td>{{bands.swir1}}</td></tr>
-  <tr><td><b>swir2</b></td><td>{{bands.swir2}}</td></tr>
-  <tr><td><b>NDVI (derived)</b></td><td>{{band_derived.ndvi}}</td></tr>
-  <tr><td><b>NDWI (derived)</b></td><td>{{band_derived.ndwi}}</td></tr>
+  <tr><td><b>Blue - 490</b></td><td>{{bands.blue}}</td></tr>
+  <tr><td><b>Green - 560</b></td><td>{{bands.green}}</td></tr>
+  <tr><td><b>Red - 660</b></td><td>{{bands.red}}</td></tr>
+  <tr><td><b>Near Infrared (NIR) - 840</b></td><td>{{bands.nir}}</td></tr>
+  <tr><td><b>Shortwave Infrared (SWIR) - 1650</b></td><td>{{bands.swir1}}</td></tr>
+  <tr><td><b>Shortwave Infrared (SWIR) - 2220</b></td><td>{{bands.swir2}}</td></tr>
+  <tr><td><b>NDVI - Red, NIR</b></td><td>{{band_derived.ndvi}}</td></tr>
+  <tr><td><b>NDWI - Green, SWIR</b></td><td>{{band_derived.ndwi}}</td></tr>
 </table>
 
 <p>
@@ -25,12 +25,12 @@ tr -d '\n' > ls57_reflectance-chart.json <<EOF
  column-units='Wavelength (nm), Reflectance x 10k'
  preview-x-label='NBART Reflectance'
 >nm,NBART Reflectance\n
-478,{{bands.blue}}\n
+490,{{bands.blue}}\n
 560,{{bands.green}}\n
-662,{{bands.red}}\n
-835,{{bands.nir}}\n
-1648,{{bands.swir1}}\n
-2205,{{bands.swir2}}
+660,{{bands.red}}\n
+840,{{bands.nir}}\n
+1650,{{bands.swir1}}\n
+2220,{{bands.swir2}}
 </chart>
 </p>
 
--- dev/terria/s2_reflectance_chart_generator.sh
+++ dev/terria/s2_reflectance_chart_generator.sh
@@ -9,19 +9,19 @@ tr -d '\n' > s2_reflectance-chart.json <<EOF
 "template": "
 <table>
   <tr><td><b>Time</b></td><td>{{time}}</td></tr>
-  <tr><td><b>nbart_coastal_aerosol</b></td><td>{{bands.nbart_coastal_aerosol}}</td></tr>
-  <tr><td><b>nbart_blue</b></td><td>{{bands.nbart_blue}}</td></tr>
-  <tr><td><b>nbart_green</b></td><td>{{bands.nbart_green}}</td></tr>
-  <tr><td><b>nbart_red</b></td><td>{{bands.nbart_red}}</td></tr>
-  <tr><td><b>nbart_red_edge_1</b></td><td>{{bands.nbart_red_edge_1}}</td></tr>
-  <tr><td><b>nbart_red_edge_2</b></td><td>{{bands.nbart_red_edge_2}}</td></tr>
-  <tr><td><b>nbart_red_edge_3</b></td><td>{{bands.nbart_red_edge_3}}</td></tr>
-  <tr><td><b>nbart_nir_1</b></td><td>{{bands.nbart_nir_1}}</td></tr>
-  <tr><td><b>nbart_nir_2</b></td><td>{{bands.nbart_nir_2}}</td></tr>
-  <tr><td><b>nbart_swir_2</b></td><td>{{bands.nbart_swir_2}}</td></tr>
-  <tr><td><b>nbart_swir_3</b></td><td>{{bands.nbart_swir_3}}</td></tr>
-  <tr><td><b>NDVI (derived)</b></td><td>{{band_derived.ndvi}}</td></tr>
-  <tr><td><b>NDWI (derived)</b></td><td>{{band_derived.ndwi}}</td></tr>
+  <tr><td><b>Narrow Blue - 440</b></td><td>{{bands.nbart_coastal_aerosol}}</td></tr>
+  <tr><td><b>Blue - 490</b></td><td>{{bands.nbart_blue}}</td></tr>
+  <tr><td><b>Green - 560</b></td><td>{{bands.nbart_green}}</td></tr>
+  <tr><td><b>Red - 670</b></td><td>{{bands.nbart_red}}</td></tr>
+  <tr><td><b>Vegetation Red Edge - 710</b></td><td>{{bands.nbart_red_edge_1}}</td></tr>
+  <tr><td><b>Vegetation Red Edge - 740</b></td><td>{{bands.nbart_red_edge_2}}</td></tr>
+  <tr><td><b>Vegetation Red Edge - 780</b></td><td>{{bands.nbart_red_edge_3}}</td></tr>
+  <tr><td><b>Near Infrared (NIR) - 840</b></td><td>{{bands.nbart_nir_1}}</td></tr>
+  <tr><td><b>Narrow Near Infrared - 870</b></td><td>{{bands.nbart_nir_2}}</td></tr>
+  <tr><td><b>Shortwave Infrared (SWIR) - 1610</b></td><td>{{bands.nbart_swir_2}}</td></tr>
+  <tr><td><b>Shortwave Infrared (SWIR) - 2190</b></td><td>{{bands.nbart_swir_3}}</td></tr>
+  <tr><td><b>NDVI - Red, NIR</b></td><td>{{band_derived.ndvi}}</td></tr>
+  <tr><td><b>NDWI - Green, NIR</b></td><td>{{band_derived.ndwi}}</td></tr>
 </table>
 
 <p>
@@ -30,15 +30,15 @@ tr -d '\n' > s2_reflectance-chart.json <<EOF
  column-units='Wavelength (nm), Reflectance x 10k'
  preview-x-label='NBART Reflectance'
 >nm,NBART Reflectance\n
-443,{{bands.nbart_coastal_aerosol}}\n
+440,{{bands.nbart_coastal_aerosol}}\n
 490,{{bands.nbart_blue}}\n
 560,{{bands.nbart_green}}\n
-665,{{bands.nbart_red}}\n
-705,{{bands.nbart_red_edge_1}}\n
+670,{{bands.nbart_red}}\n
+710,{{bands.nbart_red_edge_1}}\n
 740,{{bands.nbart_red_edge_2}}\n
-783,{{bands.nbart_red_edge_3}}\n
-842,{{bands.nbart_nir_1}}\n
-865,{{bands.nbart_nir_2}}\n
+780,{{bands.nbart_red_edge_3}}\n
+840,{{bands.nbart_nir_1}}\n
+870,{{bands.nbart_nir_2}}\n
 1610,{{bands.nbart_swir_2}}\n
 2190,{{bands.nbart_swir_3}}
 </chart>
