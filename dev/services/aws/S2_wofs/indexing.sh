80781c9645b327564cce84a023c4e6212499bec3
Duncan Gray <duncan.gray@ga.gov.au>
2020-02-19 15:24:11 +1100

    switching back.  That did not fix things

--- dev/services/aws/S2_wofs/indexing.sh
+++ dev/services/aws/S2_wofs/indexing.sh
@@ -1,3 +1,4 @@
-#datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/dsg2/dev/products/wofs/ga_s2am_wofs_2.yaml
+datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/dsg2/dev/products/wofs/ga_s2am_wofs_2.yaml #
+
 
 datacube dataset add --confirm-ignore-lineage s3://dea-public-data-dev/wofs/ga_s2am_wofs_2/54/HUF/2018/01/02/20180102T034621/ga_s2am_wofs_2-2-1_54HUF_2018-01-02_interim.odc-metadata.yaml
