623e61e212491e8fe21105c653ed33dc1b8578ab
Duncan Gray <duncan.gray@ga.gov.au>
2020-02-21 12:35:01 +1100

    indexing all of the data

--- dev/services/aws/S2_wofs/indexing.sh
+++ dev/services/aws/S2_wofs/indexing.sh
@@ -1,7 +1,8 @@
 datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/dsg2/dev/products/wofs/sentinel2_wofs_def.yaml #
 
-s3-find 's3://dea-public-data-dev/wofs/ga_s2am_wofs_2/54/HUF/2018/01/02/*/*.odc-metadata.yaml'  | s3-to-tar --xz > two_wofls.tar.xz
-dc-index-from-tar --xz --ignore-lineage --eo3 two_wofls.tar.xz
+#s3-find 's3://dea-public-data-dev/wofs/ga_s2am_wofs_2/54/HUF/2018/01/02/*/*.odc-metadata.yaml'  | s3-to-tar --xz > two_wofls.tar.xz
+s3-find 's3://dea-public-data-dev/wofs/ga_s2am_wofs_2/*/*/*/*/*/*/*.odc-metadata.yaml'  | s3-to-tar --xz > test_wofls.tar.xz
+dc-index-from-tar --xz --ignore-lineage --eo3 test_wofls.tar.xz
 
 #tar c ga_s2am_wofs_2-2-1_54HUF_2018-01-02_interim.odc-metadata.yaml | dc-index-from-tar -E  odc_conf_test --protocol file --eo3
 
