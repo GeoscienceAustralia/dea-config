c4b071435ba7c0c1de8a284015e2caba63f7d548
Vanessa Newey <vnewey@users.noreply.github.com>
2020-11-27 18:02:45 +1100

    Update ows_cfg.py
    Several changes to the TCW Percentiles data description

--- dev/services/wms/ows/ows_cfg.py
+++ dev/services/wms/ows/ows_cfg.py
@@ -5303,14 +5303,16 @@ For service status information, see https://status.dea.ga.gov.au""",
                             "title": "DEA Wetness Percentiles (Landsat)",
                             "name": "ga_ls_tcw_percentiles_2",
                             "abstract": """
-Geoscience Australia Landsat Collection 2 Tasselled Cap Wetness Percentiles 1986-2018, 25 metre, 100km tile, Australian Albers Equal Area projection (EPSG:3577). Data is only visible at higher resolutions; when zoomed-out the available area will be displayed as a shaded region.
-Areas that are partially covered in water, or where water is mixed with vegetation when viewed from above provide habitat for a wide range of aquatic organisms.
+Geoscience Australia Landsat Collection 2 Tasseled Cap Wetness Percentiles 1986-2018, 25 metre, 100km tile, Australian Albers Equal Area projection (EPSG:3577). 
+Data is only visible at higher resolutions; when zoomed-out the available area will be displayed as a shaded region.
 
-The ability to map partial inundation is also crucial to understand patterns of human water use. We need to be able to identify potential wetlands and groundwater dependent ecosystems on the Australian continent so that they can be monitored and managed.
+Areas that are partially covered in water, or where water is mixed with vegetation when viewed from above, provide habitat for a wide range of aquatic organisms. 
 
-The tasseled cap wetness percentiles provide a multi-decadal summary of landscape wetness that can be used to identify wetlands and groundwater ecosystems.
+The ability to map partial inundation is also crucial to understand patterns of human water use. We need to be able to identify potential wetlands and groundwater dependent ecosystems on the Australian continent so that they can be monitored and managed
 
-They provide statistical summaries (10th, 50th and 90th percentiles) of the tasseled cap wetness index from 1987 to 2017.
+The Tasseled Cap Wetness Percentiles provide a multi-decadal summary of landscape wetness that can be used to identify wetlands and groundwater ecosystems.
+
+They provide statistical summaries (10th, 50th and 90th percentiles) of the Tasseled Cap wetness index from 1987 to 2017.
 
 They are intended for use as inputs into classification algorithms to identify potential wetlands and groundwater dependent ecosystems, and characterise salt flats, clay pans, salt lakes and coastal land forms.
 
@@ -5321,15 +5323,18 @@ salt flats
 salt lakes
 coastal land cover classes
 
-The tasseled cap wetness transform translates the six spectral bands of Landsat into a single wetness index. The wetness index can be used to identify areas in the landscape that are potentially wetlands or groundwater dependent ecosystems. The tasseled cap wetness percentiles captures how the wetness index behaves over time. The percentiles are well suited to characterising wetlands, salt flats/salt lakes and coastal ecosystems. However, care should be applied when analysing the wetness index, as soil colour and fire scars can cause misleading results.
+The Tasseled Cap wetness transform translates the six spectral bands of Landsat into a single wetness index.  The wetness index can be used to identify areas in the landscape that are potentially wetlands or groundwater dependent ecosystems. The Tasseled Cap Wetness Percentiles capture how the wetness index behaves over time.  The percentiles are well suited to characterising wetlands, salt flats/salt lakes and coastal ecosystems. However, care should be applied when analysing the wetness index, as soil colour and fire scars can cause misleading results. In areas of high relief caused by cliffs or steep terrain, terrain shadows can cause false positives (a falsely high wetness index).
 
-The 10th, 50th and 90th percentiles of the tasseled cap wetness index are intended to capture the extreme (10th and 90th percentile) values and long term average (50th percentile) values of the wetness index. Percentiles are used in preference to minimum, maximum and mean, as the min/max/mean statistical measures are more sensitive to undetected cloud/cloud shadow, and can be misleading for non-normally distributed data.
+The 10th, 50th and 90th percentiles of the Tasseled Cap wetness index are intended to capture the extreme (10th and 90th percentile) values and long-term average (50th percentile) values of the wetness index.  Percentiles are used in preference to minimum, maximum and mean, as the min/max/mean statistical measures are more sensitive to undetected cloud/cloud shadow, and can be misleading for non-normally distributed data.
 
-The tasseled cap wetness percentiles are intended to complement the Water Observations from Space (WOfS) algorithm. WOfS is designed to discriminate open water, but the tasseled cap wetness identifies areas of water and areas where water and vegetation are mixed together; i.e. mangroves and palustrine wetlands.
+The Tasseled Cap Wetness Percentiles are intended to complement the Water Observations from Space (WOfS) algorithm. WOfS is designed to discriminate open water, but the Tasseled Cap wetness index identifies areas of water and areas where water and vegetation are mixed together; i.e. mangroves and palustrine wetlands.
 
 If you are interested in terrestrial vegetation (where water in the pixel is not a factor), use the Fractional Cover product, which provides a better biophysical characterisation of green vegetation fraction, dry vegetation fraction and bare soil vegetation fraction.
 
-We used the tasseled cap transforms described in Crist et al. (1985).
+In terms of limitations, caution should be used, especially with the Tasseled Cap wetness index results in areas where residual terrain shadow, or dark soils can cause high 'wetness' index values.
+One of the limitations of using the Tasseled Cap wetness index is that it will identify all 'wet' things, including potential wetlands, groundwater dependent ecosystems, irrigated crops/pasture, man-made water storages and sewerage treatment, and does not discriminate between these. As such it should be used in conjunction with other contextual data to ensure that features identified using the Tasseled Cap Wetness Percentiles are features of interest rather than false positives.
+
+We used the Tasseled Cap transforms described in Crist et al. (1985).
 
 Crist, E. P. (1985). A TM Tasseled Cap equivalent transformation for reflectance factor data. Remote Sensing of Environment, 17(3), 301–306. https://doi.org/10.1016/0034-4257(85)90102-6
 
