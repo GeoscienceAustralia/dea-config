237049a15a36f08bc265ea762298ab5b2bdc8fad
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-02-17 05:16:19 +0000

    setup dev_only layers folder for dev ows_cfg

--- dev/services/wms/ows_refactored/ows_root_cfg.py
+++ dev/services/wms/ows_refactored/ows_root_cfg.py
@@ -39,11 +39,6 @@ bands_weathering = {
 }
 
 
-bands_hap = {
-    "Band_1": [],
-}
-
-
 # Reusable Chunks 4. Styles
 
 
@@ -127,20 +122,6 @@ style_wii = {
         "strip_location": [0.1, 0.5, 0.8, 0.15],
     },
 }
-
-
-style_hap_simple_gray = {
-    "name": "simple_gray",
-    "title": "Simple gray",
-    "abstract": "Simple grayscale image",
-    "components": {
-        "red": {"Band_1": 1.0},
-        "green": {"Band_1": 1.0},
-        "blue": {"Band_1": 1.0},
-    },
-    "scale_range": [0.0, 255],
-}
-
 
 # End of Reuseable
 
@@ -363,7 +344,7 @@ For more information see http://pid.geoscience.gov.au/dataset/ga/122229
                             "type": "python",
                         },
                         {
-                            "include": "ows_refactored.wofs.ows_nrt_wo_cfg.layers",
+                            "include": "ows_refactored.dev_only.ows_nrt_wo_cfg.layers",
                             "type": "python",
                         },
                     ],
@@ -522,34 +503,8 @@ For service status information, see https://status.dea.ga.gov.au""",
                     "type": "python",
                 },
                 {
-                    "title": "Historical Airborne Photography",
-                    "abstract": "",
-                    "layers": [
-                        {
-                            "title": "Historical Airborne Photography (HAP)",
-                            "name": "historical_airborne_photography",
-                            "abstract": "Historical Airborne Photography",
-                            "product_name": "historical_airborne_photography",
-                            "bands": bands_hap,
-                            "resource_limits": reslim_wms_min_zoom_500_max_datasets,
-                            "image_processing": {
-                                "extent_mask_func": "datacube_ows.ogc_utils.mask_by_val",
-                                "always_fetch_bands": [],
-                                "manual_merge": False,
-                            },
-                            "wcs": {
-                                "native_crs": "EPSG:3577",
-                                "default_bands": ["Band_1"],
-                                "native_resolution": [1.0, 1.0],
-                            },
-                            "styling": {
-                                "default_style": "simple_gray",
-                                "styles": [
-                                    style_hap_simple_gray,
-                                ],
-                            },
-                        }
-                    ],
+                    "include": "ows_refactored.dev_only.ows_hap_cfg.layers",
+                    "type": "python",
                 },
                 {
                     "include": "ows_refactored.aster.ows_aster_cfg.layers",
