afe6229a1be3ed826adc3c5a96c4bad92d926ef4
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-03-17 05:33:56 +0000

    add fmask to wi

--- dev/services/wms/ows_refactored/c3/ows_c3_cfg.py
+++ dev/services/wms/ows_refactored/c3/ows_c3_cfg.py
@@ -84,6 +84,7 @@ style_c3_true_colour = {
             "invert": True,
         }
     ],
+    "additional_bands": ["oa_fmask"],
 }
 
 style_c3_false_colour = {
@@ -103,6 +104,7 @@ style_c3_false_colour = {
             "invert": True,
         }
     ],
+    "additional_bands": ["oa_fmask"],
 }
 
 style_c3_ndvi = {
@@ -128,6 +130,20 @@ style_c3_ndvi = {
         {"value": 0.8, "color": "#529400"},
         {"value": 0.9, "color": "#237100"},
         {"value": 1.0, "color": "#114D04"},
+    ],
+    "pq_masks": [
+        {
+            "band": "oa_fmask",
+            "enum": 1,
+        },
+        {
+            "band": "oa_fmask",
+            "enum": 4,
+        },
+        {
+            "band": "oa_fmask",
+            "enum": 5,
+        },
     ],
     "legend": legend_idx_0_1_5ticks,
 }
@@ -163,6 +179,20 @@ style_c3_ndwi = {
             "color": "#08306b",
         },
     ],
+    "pq_masks": [
+        {
+            "band": "oa_fmask",
+            "enum": 1,
+        },
+        {
+            "band": "oa_fmask",
+            "enum": 4,
+        },
+        {
+            "band": "oa_fmask",
+            "enum": 5,
+        },
+    ],
     "legend": {
         "begin": "0.0",
         "end": "0.5",
@@ -195,6 +225,20 @@ style_c3_mndwi = {
         {"value": 0.6, "color": "#3e8ec4"},
         {"value": 0.8, "color": "#1563aa"},
         {"value": 1.0, "color": "#08306b"},
+    ],
+    "pq_masks": [
+        {
+            "band": "oa_fmask",
+            "enum": 1,
+        },
+        {
+            "band": "oa_fmask",
+            "enum": 4,
+        },
+        {
+            "band": "oa_fmask",
+            "enum": 5,
+        },
     ],
     "legend": legend_idx_0_1_5ticks,
 }
