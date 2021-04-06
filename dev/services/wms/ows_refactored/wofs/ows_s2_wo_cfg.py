ead13c3e47e6fefbcc2797efe66bf76e7ac588f4
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-04-06 12:04:08 +1000

    Added S2 WO config

---
+++ dev/services/wms/ows_refactored/wofs/ows_s2_wo_cfg.py
@@ -0,0 +1,193 @@
+from ows_refactored.ows_reslim_cfg import reslim_wms_min_zoom_35
+from ows_refactored.wofs.bands_wo_cfg import bands_wofs_obs
+
+style_c3_wofs_obs = {
+    "name": "observations",
+    "title": "Observations",
+    "abstract": "Observations",
+    "value_map": {
+        "water": [
+            {
+                "title": "",
+                "abstract": "",
+                "flags": {
+                    "and": {
+                        "noncontiguous": True,
+                        "low_solar_angle": True
+                    }
+                },
+                "alpha": 0.0,
+                "color": "#707070",
+            },
+            {
+                "title": "Cloudy Steep Terrain",
+                "abstract": "",
+                "flags": {
+                    "and": {
+                        "cloud": True,
+                        "high_slope": True
+                    }
+                },
+                "color": "#f2dcb4",
+            },
+            {
+                "title": "Cloudy Water",
+                "abstract": "",
+                "flags": {
+                    "and": {
+                        "wet": True,
+                        "cloud": True
+                    }
+                },
+                "color": "#bad4f2",
+            },
+            {
+                "title": "Shaded Water",
+                "abstract": "",
+                "flags": {
+                    "and": {
+                        "wet": True,
+                        "cloud_shadow": True
+                    }
+                },
+                "color": "#335277",
+            },
+            {
+                "title": "Cloud",
+                "abstract": "",
+                "flags": {"cloud": True},
+                "color": "#c2c1c0",
+            },
+            {
+                "title": "Cloud Shadow",
+                "abstract": "",
+                "flags": {"cloud_shadow": True},
+                "color": "#4b4b37",
+            },
+            {
+                "title": "Terrain Shadow",
+                "abstract": "",
+                "flags": {"terrain_shadow": True},
+                "color": "#2f2922",
+            },
+            {
+                "title": "Steep Terrain",
+                "abstract": "",
+                "flags": {"high_slope": True},
+                "color": "#776857",
+            },
+            {
+                "title": "Water",
+                "abstract": "",
+                "flags": {"wet": True},
+                "color": "#4F81BD",
+            },
+            {
+                "title": "Dry",
+                "abstract": "",
+                "flags": {
+                    "dry": True,
+                },
+                "color": "#96966e",
+            },
+        ],
+    },
+    "pq_masks": [
+        {
+            "band": "land",
+            "invert": True,
+            "enum": 0,
+        }
+    ],
+    "legend": {"width": 3.0, "height": 2.1},
+}
+
+style_s2_wofs_obs_wet_only = {
+    "name": "wet",
+    "title": "Wet Only",
+    "abstract": "Wet Only",
+    "value_map": {
+        "water": [
+            {
+                "title": "Invalid",
+                "abstract": "Slope or Cloud",
+                "flags": {
+                    "or": {
+                        "terrain_shadow": True,
+                        "low_solar_angle": True,
+                        "cloud_shadow": True,
+                        "cloud": True,
+                        "high_slope": True,
+                        "noncontiguous": True,
+                    }
+                },
+                "color": "#707070",
+                "alpha": 0.0,
+            },
+            {
+                # Possible Sea Glint, also mark as invalid
+                "title": "Dry",
+                "abstract": "Dry",
+                "flags": {
+                    "dry": True,
+                },
+                "color": "#D99694",
+                "alpha": 0.0,
+            },
+            {
+                "title": "Wet",
+                "abstract": "Wet",
+                "flags": {"wet": True},
+                "color": "#4F81BD",
+            },
+        ],
+    },
+    "pq_masks": [
+        {
+            "band": "land",
+            "invert": True,
+            "enum": 1,
+        },
+    ],
+}
+
+
+layers = {
+    "title": "Sentinel 2 Water Observations",
+    "name": "ga_s2_wo_3",
+    "abstract": """ Prototype Sentinel 2 Water Observations""",
+    "product_name": "ga_s2_wo_3",
+    "bands": bands_wofs_obs,
+    "resource_limits": reslim_wms_min_zoom_35,
+    "dynamic": True,
+    "flags": [
+        {
+            "band": "land",
+            "product": "geodata_coast_100k",
+            "ignore_time": True,
+            "ignore_info_flags": [],
+        },
+        {
+            "band": "water",
+            "product": "ga_ls_wo_3",
+            "ignore_time": False,
+            "ignore_info_flags": [],
+            "fuse_func": "datacube_ows.wms_utils.wofls_fuser",
+        },
+    ],
+    "image_processing": {
+        "extent_mask_func": "datacube_ows.ogc_utils.mask_by_bitflag",
+        "always_fetch_bands": [],
+        "manual_merge": False,
+        "fuse_func": "datacube_ows.wms_utils.wofls_fuser",
+    },
+    "wcs": {
+        "native_crs": "EPSG:3577",
+        "native_resolution": [10, -10],
+        "default_bands": ["water"],
+    },
+    "styling": {
+        "default_style": "observations",
+        "styles": [style_c3_wofs_obs, style_c3_wofs_obs_wet_only],
+    },
+}
