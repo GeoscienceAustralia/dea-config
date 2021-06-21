d8731e18193a7edf924345aee0f2818764a84dfa
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-06-21 02:30:35 +0000

    fix isort

--- dev/services/wms/ows_refactored/sentinel2/ows_ard_cfg.py
+++ dev/services/wms/ows_refactored/sentinel2/ows_ard_cfg.py
@@ -1,5 +1,6 @@
 from ows_refactored.ows_reslim_cfg import reslim_wms_min_zoom_35
-from ows_refactored.sentinel2.style_s2_cfg import styles_s2_list, bands_sentinel2
+from ows_refactored.sentinel2.style_s2_cfg import (bands_sentinel2,
+                                                   styles_s2_list)
 
 bands_sentinel2_ard_nbar = {
     "nbar_coastal_aerosol": [
