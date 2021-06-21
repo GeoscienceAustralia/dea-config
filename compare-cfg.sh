caf5a62b6cc62d0c578aacc9c1c11417d153e087
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-06-21 05:07:40 +0000

    mount csv

--- compare-cfg.sh
+++ compare-cfg.sh
@@ -7,8 +7,7 @@ set -x
 while IFS=, read -r product definition; do
   # do something... Don't forget to skip the header line!
   datacube product add $definition
-done < workspaces/dev-products.csv
-
+done < /env/config/products.csv
 datacube-ows-cfg check -i /env/config/inventory.json
 
 set +x
