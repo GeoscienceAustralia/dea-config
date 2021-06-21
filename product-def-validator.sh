432e62b0f5015bd5fe066db8e9691aa29211273a
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-06-21 07:12:44 +0000

    add more command

--- product-def-validator.sh
+++ product-def-validator.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 # Convenience script for running Travis-like checks.
 
-# set -eu
-# set -x
-
 while IFS=, read -r product definition location; do
   # do something... Don't forget to skip the header line!
   datacube product add $definition
 done < /env/config/products.csv
 
+# set -eu
+# set -x
+
+datacube product list | wc -l
+cat /env/config/products.csv | wc -l
 # set +x
