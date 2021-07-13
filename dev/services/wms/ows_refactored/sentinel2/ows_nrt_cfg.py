fa933e23da20cfe00f8a2d117f66c80c624fb995
David Gavin <GypsyBojangles@users.noreply.github.com>
2021-07-13 16:23:18 +1000

    Update ows_nrt_cfg.py

--- dev/services/wms/ows_refactored/sentinel2/ows_nrt_cfg.py
+++ dev/services/wms/ows_refactored/sentinel2/ows_nrt_cfg.py
@@ -1,6 +1,5 @@
 from ows_refactored.ows_reslim_cfg import reslim_wms_min_zoom_15_cache_rules
-from ows_refactored.sentinel2.style_s2_cfg import (style_s2_nbr_list,
-                                                   styles_s2_list)
+from ows_refactored.sentinel2.style_s2_cfg import styles_s2_list
 
 bands_sentinel2 = {
     "nbar_coastal_aerosol": ["nbar_coastal_aerosol", "nbar_narrow_blue"],
@@ -187,5 +186,5 @@ For service status information, see https://status.dea.ga.gov.au
             "nbart_blue",
         ],
     },
-    "styling": {"default_style": "simple_rgb", "styles": style_s2_nbr_list},
+    "styling": {"default_style": "simple_rgb", "styles": styles_s2_list},
 }
