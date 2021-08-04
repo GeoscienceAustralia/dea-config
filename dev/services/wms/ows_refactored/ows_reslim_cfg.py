476e5dc3f2f34a82b4e28450f7a20c2c46aa8bbf
phaesler <paul.haesler@data61.csiro.au>
2021-08-04 11:52:31 +1000

    max_datasets, not min_datasets.

--- dev/services/wms/ows_refactored/ows_reslim_cfg.py
+++ dev/services/wms/ows_refactored/ows_reslim_cfg.py
@@ -2,11 +2,11 @@
 
 dataset_cache_rules = [
     {
-        "min_datasets": 5,
+        "max_datasets": 5,
         "max_age": 60 * 60 * 24,
     },
     {
-        "min_datasets": 9,
+        "max_datasets": 9,
         "max_age": 60 * 60 * 24 * 14,
     },
 ]
@@ -65,7 +65,6 @@ reslim_wms_min_zoom_500_max_datasets = {
         "zoomed_out_fill_colour": [150, 180, 200, 160],
         "min_zoom_factor": 500.0,
         "max_datasets": 6,
-        "dataset_cache_rules": dataset_cache_rules
     },
     "wcs": {
         "max_datasets": 16,
