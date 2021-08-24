6f3f85a737016d0da0f6020eb1ea98941c40b7c4
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-08-24 03:52:13 +0000

    fix typo

--- dev/services/wms/ows_refactored/sentinel2/style_s2_cfg.py
+++ dev/services/wms/ows_refactored/sentinel2/style_s2_cfg.py
@@ -1,6 +1,6 @@
 import copy
 
-from ows.refactored.sentinel2.style_s2_pure_cfg import (
+from ows_refactored.sentinel2.style_s2_pure_cfg import (
     style_s2_pure_aerosol, style_s2_pure_blue, style_s2_pure_green,
     style_s2_pure_narrow_nir, style_s2_pure_nir, style_s2_pure_red,
     style_s2_pure_redge_1, style_s2_pure_redge_2, style_s2_pure_redge_3,
