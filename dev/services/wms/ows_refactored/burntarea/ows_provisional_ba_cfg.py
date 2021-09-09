78e22dbfc04ef164b11156f3bef2abcadd83a888
Erin Telfer <Erin.Telfer@ga.gov.au>
2021-09-09 16:39:25 +1200

    reformat

--- dev/services/wms/ows_refactored/burntarea/ows_provisional_ba_cfg.py
+++ dev/services/wms/ows_refactored/burntarea/ows_provisional_ba_cfg.py
@@ -12,10 +12,10 @@ style_dbsi = {
     "name": "ga_s2_dbsi_provisional_3",
     "title": "Delta Bare Soil Index (Provisional, NRT Sentinel 2)",
     "abstract": """
-        Delta Bare Soil Index (dBSI) based on Sentinel 2 Near Real-Time (Provisional) Surface Reflectance and Sentinel 2 Barest Earth Data
+        Delta Bare Soil Index (dBSI) based on Sentinel 2 Near Real-Time (Provisional) Surface Reflectance and Sentinel 2 Barest Earth Data.
 
         Bare Soil Index (BSI) identifies soil or bare-land characteristics by combining blue, red, near infrared (NIR), and short wave infrared (SWIR) spectral bands. SWIR and red spectral bands can be used to identify basic soil mineralogy while blue and NIR spectral bands can help to detect vegetation.
-        BSI (Rikimaru and Miyatake 2002) is calculated as: BSI = ((SWIR2 + RED) - (NIR + BLUE)) / ((SWIR2 + RED) + (NIR + BLUE))
+        BSI (Rikimaru and Miyatake 2002) is calculated as: BSI = ((SWIR2 + RED) - (NIR + BLUE)) / ((SWIR2 + RED) + (NIR + BLUE)).
 
         Delta BSI (dBSI) shows the change in soil characteristics between two events, the difference between a baseline (pre-BSI) and a latter time (post-BSI). The change (delta) in BSI is calculated as: dBSI = pre-BSI - post-BSI.
 
@@ -34,20 +34,21 @@ style_dbsi = {
             "band": "delta_bsi",
         },
     },
-    "range": [-1,1],
+    "range": [-1, 1],
     "mpl_ramp": "YlOrBr",
-    "legend": {"url": "",  # need this
-    },
+    "legend": {
+        "url": "",  # need this
+        },
 }
 
 style_ndvi = {
     "name": "ga_s2_ndvi_provisional_3",
     "title": "Delta Normalized Difference Vegetation Index (Provisional, NRT Sentinel 2)",
     "abstract": """
-        Delta Normalized Difference Vegetation Index (dNDVI) based on Sentinel 2 Near Real-Time (Provisional) Surface Reflectance and Sentinel 2 Barest Earth Data
+        Delta Normalized Difference Vegetation Index (dNDVI) based on Sentinel 2 Near Real-Time (Provisional) Surface Reflectance and Sentinel 2 Barest Earth Data.
 
-        Normalized Difference Vegetation Index (NDVI) Normalized Difference Vegetation Index (NDVI) is used to detect green vegetation characteristics by identifying the difference between red/visible and near infrared (NIR) spectral bands. 
-        NDVI is calculated as: NDVI =  (NIR - RED)/(NIR + RED)
+        Normalized Difference Vegetation Index (NDVI) Normalized Difference Vegetation Index (NDVI) is used to detect green vegetation characteristics by identifying the difference between red/visible and near infrared (NIR) spectral bands.
+        NDVI is calculated as: NDVI =  (NIR - RED)/(NIR + RED).
 
         Delta NDVI (dNDVI) shows the change in vegetation characteristics between two events, the difference between a baseline (pre-NDVI) and a latter time (post-NDVI). The change (delta) in NDVI is calculated as: dNDVI = pre-ndvi - post-NDVI.
 
@@ -67,7 +68,7 @@ style_ndvi = {
             "band": "delta_ndvi",
         },
     },
-    "range": [-1,1], 
+    "range": [-1, 1],
     "color_ramp": [
         {"value": -1.0, "color": "#8F3F20", "alpha": 0.0},
         {"value": -1.0, "color": "#8F3F20", "alpha": 1.0},
@@ -81,7 +82,7 @@ style_ndvi = {
         {"value": 0.6, "color": "#529400"},
         {"value": 0.8, "color": "#237100"},
         {"value": 1.0, "color": "#114D04"},
-    ]
+        ]
     "legend": {
         "url": "",  # need this
     },
@@ -91,19 +92,19 @@ style_dnbr = {
     "name": "ga_s2_dnbr_provisional_3",
     "title": "Delta Normalized Burn Ratio (Provisional, NRT Sentinel 2)",
     "abstract": """
-        Delta Normalized Burn Ratio (DNBR) based on Sentinel 2 Near Real-Time (Provisional) Surface Reflectance and Sentinel 2 Barest Earth Data
+        Delta Normalized Burn Ratio (DNBR) based on Sentinel 2 Near Real-Time (Provisional) Surface Reflectance and Sentinel 2 Barest Earth Data.
 
-        Normalized Burn Ratio (NBR) identifies areas that have the characteristics of being burnt. NBR looks at the relationship between near infrared (NIR) and short wave infrared (SWIR) spectral signatures. High SWIR reflectance values with low NIR reflectance values are indicative of an area that has been burnt by fire(s), while the opposite is seen in healthy vegetation. 
-        NBR is calculated as: NBR = (NIR – SWIR2) / (NIR + SWIR2)
+        Normalized Burn Ratio (NBR) identifies areas that have the characteristics of being burnt. NBR looks at the relationship between near infrared (NIR) and short wave infrared (SWIR) spectral signatures. High SWIR reflectance values with low NIR reflectance values are indicative of an area that has been burnt by fire(s), while the opposite is seen in healthy vegetation.
+        NBR is calculated as: NBR = (NIR – SWIR2) / (NIR + SWIR2).
 
-        Delta NBR shows the change in burn characteristics between two events, the difference between a baseline (pre-NBR) and a latter time (post-NBR). The change (delta) in NBR is calculated as: dNBR = pre-NBR - post-NBR 
+        Delta NBR shows the change in burn characteristics between two events, the difference between a baseline (pre-NBR) and a latter time (post-NBR). The change (delta) in NBR is calculated as: dNBR = pre-NBR - post-NBR.
 
         Normally dNBR analysis is run on data from a clear pre-fire and a post-fire satellite scene. However, in order to rapidly identify areas that are characteristic of burns for all of Australia, and on a near real-time basis, this dNBR product is based on two input datasets:
          1) The baseline (pre) NBR is calculated on DEA Sentinel 2 Barest Earth (Roberts, et al. 2019).
          2) The latter (post) NBR is calculated on the latest daily Sentinel-2 (A and B combined) Near Real-Time provisional data. The Near Real-Time capability provides analysis-ready data that is processed on receipt using the best-available ancillary information at the time to provide atmospheric corrections.
-        Therefore, this dNBR data is a general screening tool to identify areas that are likely to have undergone burn. dNBR is a basic metric that can be used to identify areas that show characteristics of burn, but there is no guarantee that values are precise. For example, Land-use change, such as deforestation or changes to water bodies, can often be identified as false positives.  
+        Therefore, this dNBR data is a general screening tool to identify areas that are likely to have undergone burn. dNBR is a basic metric that can be used to identify areas that show characteristics of burn, but there is no guarantee that values are precise. For example, Land-use change, such as deforestation or changes to water bodies, can often be identified as false positives.
 
-        The larger the positive dNBR value, the more the area shows characteristics of being burnt. This metric should be used in combination with the other datasets to strengthen the agreement that the area has indeed been burnt. 
+        The larger the positive dNBR value, the more the area shows characteristics of being burnt. This metric should be used in combination with the other datasets to strengthen the agreement that the area has indeed been burnt.
 
         For service status information, see https://status.dea.ga.gov.au
 
@@ -116,33 +117,32 @@ style_dnbr = {
             "band": "delta_nbr",
         },
     },
-    "range": [-1,1], 
-    "mpl_ramp": "PuOr", #need to ask how to reverse colour ramp
+    "range": [-1, 1],
+    "mpl_ramp": "PuOr",  # need to ask how to reverse colour ramp
     "legend": {
-        "url": "", #need this
+        "url": "",  # need this
     },
 }
 
 style_dnbr_classes = {
     "name": "ga_s2_dnbrclasses_provisional_3",
-    "title": "Delta Normalized Burn Ratio Classes (Provisional, NRT Sentinel 2),
-    "abstract": """ 
-        Delta Normalized Burn Ratio (DNBR) based on Sentinel 2 Near Real-Time (Provisional) Surface Reflectance and Sentinel 2 Barest Earth Data
-        Values are divded into levels according to
+    "title": "Delta Normalized Burn Ratio Classes (Provisional, NRT Sentinel 2)",
+    "abstract": """
+        Delta Normalized Burn Ratio (DNBR) based on Sentinel 2 Near Real-Time (Provisional) Surface Reflectance and Sentinel 2 Barest Earth Data.
 
-        Burn classes and thresholds are a simplified version of those proposed by USGS. Color coding established by UN-SPIDER
+        Burn classes and thresholds are a simplified version of those proposed by USGS. Color coding established by UN-SPIDER.
 
-        Normalized Burn Ratio (NBR) identifies areas that have the characteristics of being burnt. NBR looks at the relationship between near infrared (NIR) and short wave infrared (SWIR) spectral signatures. High SWIR reflectance values with low NIR reflectance values are indicative of an area that has been burnt by fire(s), while the opposite is seen in healthy vegetation. 
-        NBR is calculated as: NBR = (NIR – SWIR2) / (NIR + SWIR2)
+        Normalized Burn Ratio (NBR) identifies areas that have the characteristics of being burnt. NBR looks at the relationship between near infrared (NIR) and short wave infrared (SWIR) spectral signatures. High SWIR reflectance values with low NIR reflectance values are indicative of an area that has been burnt by fire(s), while the opposite is seen in healthy vegetation.
+        NBR is calculated as: NBR = (NIR – SWIR2) / (NIR + SWIR2).
 
-        Delta NBR (dNBR) shows the change in burn characteristics between two events, the difference between a baseline (pre-NBR) and a latter time (post-NBR). The change (delta) in NBR is calculated as: dNBR = pre-NBR - post-NBR 
+        Delta NBR (dNBR) shows the change in burn characteristics between two events, the difference between a baseline (pre-NBR) and a latter time (post-NBR). The change (delta) in NBR is calculated as: dNBR = pre-NBR - post-NBR.
 
         Normally dNBR analysis is run on data from a clear pre-fire and a post-fire satellite scene. However, in order to rapidly identify areas that are characteristic of burns for all of Australia, and on a near real-time basis, this dNBR product is based on two input datasets:
          1) The baseline (pre) NBR is calculated on DEA Sentinel 2 Barest Earth (Roberts, et al. 2019).
          2) The latter (post) NBR is calculated on the latest daily Sentinel-2 (A and B combined) Near Real-Time provisional data. The Near Real-Time capability provides analysis-ready data that is processed on receipt using the best-available ancillary information at the time to provide atmospheric corrections.
-        Therefore, this dNBR data is a general screening tool to identify areas that are likely to have undergone burn. dNBR is a basic metric that can be used to identify areas that show characteristics of burn, but there is no guarantee that values are precise. For example, Land-use change, such as deforestation or changes to water bodies, can often be identified as false positives.  
+        Therefore, this dNBR data is a general screening tool to identify areas that are likely to have undergone burn. dNBR is a basic metric that can be used to identify areas that show characteristics of burn, but there is no guarantee that values are precise. For example, Land-use change, such as deforestation or changes to water bodies, can often be identified as false positives.
 
-        The larger the positive dNBR value, the more the area shows characteristics of being burnt. This metric should be used in combination with the other datasets to strengthen the agreement that the area has indeed been burnt. 
+        The larger the positive dNBR value, the more the area shows characteristics of being burnt. This metric should be used in combination with the other datasets to strengthen the agreement that the area has indeed been burnt.
 
         For service status information, see https://status.dea.ga.gov.au
         """,
@@ -154,11 +154,11 @@ style_dnbr_classes = {
             "band": "delta_bsi",
         },
     },
-    "range": [-1,1],
+    "range": [-1, 1],
     "color_ramp": [
         {"value": -1.0, "color": "#0be344",},
         {"value": 0.1, "color": "#0be344",},
-        {"value": 0.05, "color": "#f8fc11", },
+        {"value": 0.05, "color": "#f8fc11",},
         {"value": 0.1, "color": "#f8671a",},
         {"value": 1.0, "color": "#f8671a"},
     ],
