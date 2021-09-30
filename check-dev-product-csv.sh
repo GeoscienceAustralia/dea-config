04a1c3264be57d68c4ff2af01755aeac09dbef97
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-09-30 03:52:41 +0000

    add csv check

--- check-dev-product-csv.sh
+++ check-dev-product-csv.sh
@@ -11,9 +11,9 @@ while IFS=, read -r product definition location; do
     if [[ $definition == *"http"* ]]; then
         datacube product add $definition
     fi
-done < /env/config/prod-products.csv
+done < /env/config/dev-products.csv
 
-if [ $(datacube product list | wc -l) -eq 69 ]; then
+if [ $(datacube product list | wc -l) -eq 74 ]; then
     echo 'equal'
 fi
 
