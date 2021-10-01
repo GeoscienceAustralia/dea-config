7b95d51f7a57a43561cc119bc444ec26ef79aaaf
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-10-01 00:05:14 +0000

    seperate metadata add

--- check-prod-product-csv.sh
+++ check-prod-product-csv.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-# Convenience script for running Travis-like checks.
 
 set -eu
 set -x
-
-datacube metadata add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/41a6cdcd08043e9b2ee292024320db175a86c9d2/workspaces/sandbox-metadata.yaml
-datacube metadata add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/nrt/sentinel/eo_s2_nrt.odc-type.yaml
 
 while IFS=, read -r product definition location; do
     if [[ $definition == *"http"* ]]; then
--- check-product-definitions.sh
+++ check-product-definitions.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
-# Convenience script for running Travis-like checks.
 
 set -eu
 set -x
 
-# datacube metadata add https://raw.githubusercontent.com/GeoscienceAustralia/digitalearthau/develop/digitalearthau/config/metadata-types.odc-type.yaml
-datacube metadata add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/nrt/sentinel/eo_s2_nrt.odc-type.yaml
-
-for prod_def_yaml in /env/config/products/*; do
-    datacube product add prod_def_yaml
+for prod_def_yaml in $(find /env/config/products -name '*.yaml'); do
+    if [[ $prod_def_yaml != "/env/config/products/baseline_satellite_data/nrt/sentinel/eo_s2_nrt.odc-type.yaml" && $prod_def_yaml != "/env/config/products/land_and_vegetation/mangrove/mangrove.yaml" ]]; then
+        datacube product add $prod_def_yaml
+    fi
 done
 
 if [ $(datacube product list | wc -l) -eq 74 ]; then
