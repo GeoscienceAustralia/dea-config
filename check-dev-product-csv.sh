93d2a2fa643818bc9bc68e657f83b6b852494b68
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-10-01 00:49:02 +0000

    fix shell

--- check-dev-product-csv.sh
+++ check-dev-product-csv.sh
@@ -10,8 +10,8 @@ while IFS=, read -r product definition location; do
     fi
 done < /env/config/dev-products.csv
 
-if [ $(datacube product list | wc -l) -eq 74 ]; then
-    echo 'equal'
+if [ $(datacube product list | wc -l) -ne 74 ]; then
+    exit 1
 fi
 
 set +x
--- check-prod-product-csv.sh
+++ check-prod-product-csv.sh
@@ -9,8 +9,8 @@ while IFS=, read -r product definition location; do
     fi
 done < /env/config/prod-products.csv
 
-if [ $(datacube product list | wc -l) -eq 69 ]; then
-    echo 'equal'
+if [ $(datacube product list | wc -l) -ne 69 ]; then
+    exit 1
 fi
 
 set +x
