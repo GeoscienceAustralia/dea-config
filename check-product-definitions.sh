fa98d1747bcadaf8ca17eac958746fb69636de07
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-10-01 05:25:16 +0000

    fix typo on decommissioned

--- check-product-definitions.sh
+++ check-product-definitions.sh
@@ -4,7 +4,7 @@ set -eu
 set -x
 
 for prod_def_yaml in $(find /env/config/products -name '*.yaml'); do
-    if [[ $prod_def_yaml != "/env/config/products/baseline_satellite_data/nrt/sentinel/eo_s2_nrt.odc-type.yaml" && $prod_def_yaml != "/env/config/products/land_and_vegetation/mangrove/mangrove.yaml" && $prod_def_yaml != "/env/config/products/land_and_vegetation/fractional-cover/ga_ls_fc_pc_cyear_3.yaml" && $prod_def_yaml != "/env/config/products/land_and_vegetation/fractional-cover/product-definition.yaml" && $prod_def_yaml != *"decomissioned/"* ]]; then
+    if [[ $prod_def_yaml != "/env/config/products/baseline_satellite_data/nrt/sentinel/eo_s2_nrt.odc-type.yaml" && $prod_def_yaml != "/env/config/products/land_and_vegetation/mangrove/mangrove.yaml" && $prod_def_yaml != "/env/config/products/land_and_vegetation/fractional-cover/ga_ls_fc_pc_cyear_3.yaml" && $prod_def_yaml != "/env/config/products/land_and_vegetation/fractional-cover/product-definition.yaml" && $prod_def_yaml != *"decommissioned/"* ]]; then
         datacube product add $prod_def_yaml
     fi
 done
