94d5cf4785710d31649ba8fe66615a0225525f7f
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-10-01 05:22:47 +0000

    exclude decommisioned folder

--- check-product-definitions.sh
+++ check-product-definitions.sh
@@ -4,12 +4,12 @@ set -eu
 set -x
 
 for prod_def_yaml in $(find /env/config/products -name '*.yaml'); do
-    if [[ $prod_def_yaml != "/env/config/products/baseline_satellite_data/nrt/sentinel/eo_s2_nrt.odc-type.yaml" && $prod_def_yaml != "/env/config/products/land_and_vegetation/mangrove/mangrove.yaml" && $prod_def_yaml != "/env/config/products/land_and_vegetation/fractional-cover/ga_ls_fc_pc_cyear_3.yaml" && $prod_def_yaml != "/env/config/products/land_and_vegetation/fractional-cover/product-definition.yaml" ]]; then
+    if [[ $prod_def_yaml != "/env/config/products/baseline_satellite_data/nrt/sentinel/eo_s2_nrt.odc-type.yaml" && $prod_def_yaml != "/env/config/products/land_and_vegetation/mangrove/mangrove.yaml" && $prod_def_yaml != "/env/config/products/land_and_vegetation/fractional-cover/ga_ls_fc_pc_cyear_3.yaml" && $prod_def_yaml != "/env/config/products/land_and_vegetation/fractional-cover/product-definition.yaml" && $prod_def_yaml != *"decomissioned/"* ]]; then
         datacube product add $prod_def_yaml
     fi
 done
 
-if [ $(datacube product list | wc -l) -ne 94 ]; then
+if [ $(datacube product list | wc -l) -ne 65 ]; then
      exit 1
 fi
 
