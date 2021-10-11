cb95f671526ffd4c875fa01eb880f472c9d3a0e4
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-10-11 04:44:02 +0000

    add products def from digitalearthau

--- check-dev-product-csv.sh
+++ check-dev-product-csv.sh
@@ -10,8 +10,9 @@ while IFS=, read -r product definition location; do
     fi
 done < /env/config/dev-products.csv
 
-if [ $(datacube product list | wc -l) -ne 76 ]; then
-    exit 1
-fi
+# if [ $(datacube product list | wc -l) -ne 76 ]; then
+#     exit 1
+# fi
+datacube product list
 
 set +x
