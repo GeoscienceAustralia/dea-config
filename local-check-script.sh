712b070f7ca30e4efe6ec4ce011e0d82a1e6fbe8
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-10-13 22:12:49 +0000

    cleanup decommissioned products folder

--- local-check-script.sh
+++ local-check-script.sh
@@ -8,11 +8,12 @@
 
 # set -eu
 # set -x
-
-datacube product list -f csv > /tmp/product_list.csv
+datacube system init
+./datacube_init_metadata.sh
 
 # SECTION 1
 # check against live db
+datacube product list -f csv > /tmp/product_list.csv
 while IFS=, read -r id name description ancillary_quality latgqa_cep90 product_type gqa_abs_iterative_mean_xy gqa_ref_source sat_path gqa_iterative_stddev_xy time sat_row orbit gqa instrument gqa_abs_xy crs resolution tile_size spatial_dimensions; do
     if [[ $(wget https://explorer.dev.dea.ga.gov.au/products/$name.odc-product.yaml -O-) ]] 2>/dev/null
         then
@@ -21,10 +22,26 @@ while IFS=, read -r id name description ancillary_quality latgqa_cep90 product_t
 done < /tmp/product_list.csv
 
 # SECTION 2
-# check against folder search for duplicates
+# check against folder search for indexed products and filter out duplicate product files
+datacube product list -f csv > /tmp/product_list.csv
 while IFS=, read -r id name description ancillary_quality latgqa_cep90 product_type gqa_abs_iterative_mean_xy gqa_ref_source sat_path gqa_iterative_stddev_xy time sat_row orbit gqa instrument gqa_abs_xy crs resolution tile_size spatial_dimensions; do
     echo $name
     grep -rnw '/env/config/products' -e "name: $name"
+done < /tmp/product_list.csv
+
+# SECTION 3
+# check for duplicates within decommissioned folder
+for prod_def_yaml in $(find /env/config/products/decommissioned ! -path '*/duplicates/*' -name '*.yaml'); do
+    datacube product add $prod_def_yaml
+done
+
+datacube product list -f csv > /tmp/product_list.csv
+
+while IFS=, read -r id name description ancillary_quality latgqa_cep90 product_type gqa_abs_iterative_mean_xy gqa_ref_source sat_path gqa_iterative_stddev_xy time sat_row orbit gqa instrument gqa_abs_xy crs resolution tile_size spatial_dimensions; do
+    echo $name
+    if [  ${#name} -ge 1 ];then
+        grep -rnw '/env/config/products/decommissioned' -e "name: $name"
+    fi
 done < /tmp/product_list.csv
 
 # set +x
