f8fe16178587f5143a81d133a5bdac03a42fa3e5
Belle Tissott <belletissott@yahoo.com>
2021-11-08 02:53:42 +0000

    remove imports for layers I haven't written yet

--- dev/services/wms/ows_refactored/inland_water/wofs/ows_wofs_cfg.py
+++ dev/services/wms/ows_refactored/inland_water/wofs/ows_wofs_cfg.py
@@ -68,10 +68,6 @@ layers = {
             "type": "python",
         },
         {
-            "include": "ows_refactored.inland_water.wofs.ows_wofs_summary_cfg.c3_statistics_layer",
-            "type": "python",
-        },
-        {
             "include": "ows_refactored.inland_water.wofs.ows_wofs_apr_oct_cfg.c3_layers",
             "type": "python",
         },
@@ -81,10 +77,6 @@ layers = {
         },
         {
             "include": "ows_refactored.inland_water.wofs.ows_wofs_annual_cfg.c3_layers",
-            "type": "python",
-        },
-        {
-            "include": "ows_refactored.inland_water.wofs.ows_wofs_summary_cfg.c3_layers",
             "type": "python",
         },
     ],
