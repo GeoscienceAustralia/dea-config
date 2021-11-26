43c8e8c0b770fe321f16371fb248bbd3d8028e7a
Erin Telfer <erin.telfer@ga.gov.au>
2021-11-26 14:18:11 +1100

    retry

--- dev/services/wms/ows_refactored/inland_water/wofs/ows_wofs_cfg.py
+++ dev/services/wms/ows_refactored/inland_water/wofs/ows_wofs_cfg.py
@@ -4,23 +4,67 @@ layers = {
     "abstract": "Digital Earth Australia (DEA) Water Observations from Space (WOfS)",
     "layers": [
         {
-            "include": "ows_refactored.inland_water.wofs.individual.ows_wofs_cfg.layers",
+            "include": "ows_refactored.inland_water.wofs.individual.ows_c3_wo_cfg.layer",
             "type": "python",
         },
         {
-            "include": "ows_refactored.inland_water.wofs.multiyear.ows_wofs_cfg.layers",
+            "include": "ows_refactored.inland_water.wofs.individual.ows_c3_wo_cfg.layer_usgsc2",
             "type": "python",
         },
         {
-            "include": "ows_refactored.inland_water.wofs.annual.ows_wofs_cfg.layers",
+            "include": "ows_refactored.inland_water.wofs.individual.ows_s2_wo_cfg.layer",
             "type": "python",
         },
         {
-            "include": "ows_refactored.inland_water.wofs.seasonal.ows_wofs_cfg.layers",
+            "include": "ows_refactored.inland_water.wofs.multiyear.ows_wofs_summary_cfg.c3_wofs_layer",
             "type": "python",
         },
         {
-            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_cfg.layers",
+            "include": "ows_refactored.inland_water.wofs.annual.ows_wofs_annual_cfg.c3_statistics_layer",
+            "type": "python",
+        },
+        {
+            "include": "ows_refactored.inland_water.wofs.seasonal.ows_wofs_apr_oct_cfg.c3_statistics_layer",
+            "type": "python",
+        },
+        {
+            "include": "ows_refactored.inland_water.wofs.seasonal.ows_wofs_nov_mar_cfg.c3_statistics_layer",
+            "type": "python",
+        },
+        {
+            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_alber_cfg.layer",
+            "type": "python",
+        },
+        {
+            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_annual_cfg.layer",
+            "type": "python",
+        },
+        {
+            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_annual_cfg.statistics_layer",
+            "type": "python",
+        },
+        {
+            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_summary_cfg.statistics_layer",
+            "type": "python",
+        },
+        {
+            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_summary_cfg.layer",
+            "type": "python",
+        },
+        {
+            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_apr_oct_cfg.statistics_layer",
+            "type": "python",
+        },
+        {
+            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_apr_oct_cfg.layers",
+            "type": "python",
+        },
+        {
+            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_nov_mar_cfg.statistics_layer",
+            "type": "python",
+        },
+        {
+            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_nov_mar_cfg.layer",
             "type": "python",
         }
     ],
