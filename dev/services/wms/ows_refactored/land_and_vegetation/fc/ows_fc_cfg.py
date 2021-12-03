f8c69200f7a24e8a8e0ea8f988b88d392abb01a2
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2021-12-03 16:08:08 +1100

    Remove space

--- dev/services/wms/ows_refactored/land_and_vegetation/fc/ows_fc_cfg.py
+++ dev/services/wms/ows_refactored/land_and_vegetation/fc/ows_fc_cfg.py
@@ -1,6 +1,6 @@
 layers = {
     "title": "DEA Fractional Cover",
-    "abstract": """ Geoscience Australia Landsat Fractional Cover
+    "abstract": """Geoscience Australia Landsat Fractional Cover
 
 Data is only visible at higher resolutions; when zoomed-out the available area will be displayed as a shaded region.
 Fractional cover provides information about the the proportions of green vegetation, non-green vegetation (including deciduous trees during autumn, dry grass, etc.), and bare areas for every 25m x 25m ground footprint. Fractional cover provides insight into how areas of dry vegetation and/or bare soil and green vegetation are changing over time. The fractional cover algorithm was developed by the Joint Remote Sensing Research Program, for more information please see data.auscover.org.au/xwiki/bin/view/Product+pages/Landsat+Fractional+Cover
