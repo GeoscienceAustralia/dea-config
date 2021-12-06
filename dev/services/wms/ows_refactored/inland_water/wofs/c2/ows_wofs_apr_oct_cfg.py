026270699732dbd7adb09a23fdc1176b031e9b9b
Erin Telfer <erin.telfer@ga.gov.au>
2021-12-06 17:00:55 +1100

    edits

--- dev/services/wms/ows_refactored/inland_water/wofs/c2/ows_wofs_apr_oct_cfg.py
+++ dev/services/wms/ows_refactored/inland_water/wofs/c2/ows_wofs_apr_oct_cfg.py
@@ -4,6 +4,151 @@ from ows_refactored.ows_legend_cfg import (legend_idx_percentage_by_20,
                                            legend_idx_twentyplus_3ticks)
 from ows_refactored.ows_reslim_cfg import reslim_wms_min_zoom_15_cache_rules
 
+style_wofs_seasonal_wet = {
+    "name": "seasonal_water_observations",
+    "title": "Seasonal Summary - Wet Count",
+    "abstract": "WOfS seasonal summary showing the count of wet observations",
+    "needed_bands": ["count_wet"],
+    "index_function": {
+        "function": "datacube_ows.band_utils.single_band",
+        "mapped_bands": True,
+        "kwargs": {
+            "band": "count_wet",
+        },
+    },
+    "color_ramp": [
+        {"value": 0, "color": "#666666", "alpha": 0},
+        {
+            # purely for legend display
+            # we should not get fractional
+            # values in this styles
+            "value": 0.2,
+            "color": "#990000",
+            "alpha": 1,
+        },
+        {"value": 2, "color": "#990000"},
+        {"value": 4, "color": "#E38400"},
+        {"value": 6, "color": "#E3DF00"},
+        {"value": 8, "color": "#00E32D"},
+        {"value": 10, "color": "#00E3C8"},
+        {"value": 12, "color": "#0097E3"},
+        {"value": 14, "color": "#005FE3"},
+        {"value": 16, "color": "#000FE3"},
+        {"value": 18, "color": "#000EA9"},
+        {
+            "value": 20,
+            "color": "#5700E3",
+        },
+    ],
+    "legend": legend_idx_twentyplus_3ticks,
+}
+
+style_wofs_seasonal_clear = {
+    "name": "seasonal_clear_observations",
+    "title": "Seasonal Summary - Clear Count",
+    "abstract": "WOfS seasonal summary showing the count of clear observations",
+    "needed_bands": ["count_clear"],
+    "index_function": {
+        "function": "datacube_ows.band_utils.single_band",
+        "mapped_bands": True,
+        "kwargs": {
+            "band": "count_clear",
+        },
+    },
+    "color_ramp": [
+        {"value": 0, "color": "#FFFFFF", "alpha": 0},
+        {
+            # purely for legend display
+            # we should not get fractional
+            # values in this styles
+            "value": 0.2,
+            "color": "#B21800",
+            "alpha": 1,
+        },
+        {"value": 1, "color": "#B21800"},
+        {"value": 4, "color": "#ef8500"},
+        {"value": 8, "color": "#ffb800"},
+        {"value": 10, "color": "#ffd000"},
+        {"value": 13, "color": "#fff300"},
+        {"value": 16, "color": "#fff300"},
+        {"value": 20, "color": "#c1ec00"},
+        {"value": 24, "color": "#6ee100"},
+        {"value": 28, "color": "#39a500"},
+        {
+            "value": 30,
+            "color": "#026900",
+        },
+    ],
+    "legend": legend_idx_thirtyplus_4ticks,
+}
+
+style_seasonal_wofs_summary_frequency = {
+    "name": "seasonal_WOfS_frequency",
+    "title": "Seasonal Summary - Frequency",
+    "abstract": "WOfS seasonal summary showing the frequency of Wetness",
+    "needed_bands": ["frequency"],
+    "index_function": {
+        "function": "datacube_ows.band_utils.single_band",
+        "mapped_bands": True,
+        "kwargs": {
+            "band": "frequency",
+        },
+    },
+    "color_ramp": [
+        {"value": 0.0, "color": "#000000", "alpha": 0.0},
+        {"value": 0.02, "color": "#000000", "alpha": 0.0},
+        {"value": 0.05, "color": "#8e0101", "alpha": 0.25},
+        {"value": 0.1, "color": "#cf2200", "alpha": 0.75},
+        {"value": 0.2, "color": "#e38400"},
+        {"value": 0.3, "color": "#e3df00"},
+        {"value": 0.4, "color": "#62e300"},
+        {"value": 0.5, "color": "#00e32d"},
+        {"value": 0.6, "color": "#00e3c8"},
+        {"value": 0.7, "color": "#0097e3"},
+        {"value": 0.8, "color": "#005fe3"},
+        {"value": 0.9, "color": "#000fe3"},
+        {"value": 1.0, "color": "#5700e3"},
+    ],
+    "legend": legend_idx_percentage_by_20,
+}
+
+style_seasonal_wofs_summary_frequency_blue = {
+    "name": "seasonal_WOfS_frequency_blues_transparent",
+    "title": "Seasonal Summary - Frequency (Blue)",
+    "abstract": "WOfS seasonal summary showing the frequency of Wetness",
+    "index_function": {
+        "function": "datacube_ows.band_utils0.single_band",
+        "mapped_bands": True,
+        "kwargs": {
+            "band": "frequency",
+        },
+    },
+    "needed_bands": ["frequency"],
+    "color_ramp": [
+        {
+            "value": 0.0,
+            "color": "#ffffff",
+            "alpha": 0.0,
+        },
+        {
+            "value": 0.001,
+            "color": "#d5fef9",
+            "alpha": 0.0,
+        },
+        {
+            "value": 0.02,
+            "color": "#d5fef9",
+        },
+        {"value": 0.2, "color": "#71e3ff"},
+        {"value": 0.4, "color": "#01ccff"},
+        {"value": 0.6, "color": "#0178ff"},
+        {"value": 0.8, "color": "#2701ff"},
+        {"value": 1.0, "color": "#5700e3"},
+    ],
+    "legend": legend_idx_percentage_by_20,
+}
+
+
 layer = {
     "title": "DEA April-October Water Observations Source Data (C2)",
     "abstract": "WOfS",
