406beb87621f018ac40ea57f74a53b29d122d3b1
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-04-06 06:05:50 +0000

    update dev ows layers

--- dev/services/wms/ows_refactored/inland_water/wofs/ows_wofs_cfg.py
+++ dev/services/wms/ows_refactored/inland_water/wofs/ows_wofs_cfg.py
@@ -32,22 +32,6 @@ layers = {
             "type": "python",
         },
         {
-            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_alber_cfg.layer",
-            "type": "python",
-        },
-        {
-            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_annual_cfg.statistics_layer",
-            "type": "python",
-        },
-        {
-            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_annual_cfg.clear_layer",
-            "type": "python",
-        },
-        {
-            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_annual_cfg.wet_layer",
-            "type": "python",
-        },
-        {
             "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_summary_cfg.statistics_layer",
             "type": "python",
         },
@@ -65,30 +49,6 @@ layers = {
         },
         {
             "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_summary_cfg.filtered_layer",
-            "type": "python",
-        },
-        {
-            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_apr_oct_cfg.statistics_layer",
-            "type": "python",
-        },
-        {
-            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_apr_oct_cfg.clear_layer",
-            "type": "python",
-        },
-        {
-            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_apr_oct_cfg.wet_layer",
-            "type": "python",
-        },
-        {
-            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_nov_mar_cfg.statistics_layer",
-            "type": "python",
-        },
-        {
-            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_nov_mar_cfg.clear_layer",
-            "type": "python",
-        },
-        {
-            "include": "ows_refactored.inland_water.wofs.c2.ows_wofs_nov_mar_cfg.wet_layer",
             "type": "python",
         }
     ],
