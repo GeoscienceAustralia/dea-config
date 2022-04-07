bb398ef743809c427f4dcc9988bf77342b5b479e
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-04-07 06:08:33 +0000

    sleep more;

--- check-coverage.sh
+++ check-coverage.sh
@@ -2,12 +2,14 @@
 # Convenience script for running Travis-like checks.
 set -ex
 
+datacube system init 
+
 # Run tests, taking coverage.
 sleep 3
 # Users can specify extra folders as arguments.
 
-datacube system init 
 
+sleep 1
 # run test
 python3 -m pytest --cov=ows_refactored --cov-report=xml coverage_test/cfg_parser.py
 cp /tmp/coverage.xml /mnt/artifacts
