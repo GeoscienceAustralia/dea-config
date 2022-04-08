c3392ac98732a523da028ad901f273e744f0f4b0
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-04-08 01:26:15 +0000

    add more coverage tests

--- coverage_test/cfg_parser.py
+++ coverage_test/cfg_parser.py
@@ -14,3 +14,18 @@ from ows_refactored.ows_root_cfg import ows_cfg
 def test_cfg_parser_simple(runner):
     result = runner.invoke(main, [])
     assert result.exit_code == 0
+
+def test_cfg_parser_parse_only(runner):
+    result = runner.invoke(main, ["check", "-p"])
+    print(repr(result))
+    assert result.exit_code == 0
+
+
+def test_cfg_parser_folder_hierarchy(runner):
+    result = runner.invoke(main, ["check", "-f"])
+    assert result.exit_code == 0
+
+
+def test_cfg_parser_styles(runner):
+    result = runner.invoke(main, ["check", "-s"])
+    assert result.exit_code == 0
