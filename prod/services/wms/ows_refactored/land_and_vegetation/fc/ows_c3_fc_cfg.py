979ade96383a8025d47958612a5e2f4af2ad3841
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-05-23 23:07:49 +0000

    revert c3 fc change

--- prod/services/wms/ows_refactored/land_and_vegetation/fc/ows_c3_fc_cfg.py
+++ prod/services/wms/ows_refactored/land_and_vegetation/fc/ows_c3_fc_cfg.py
@@ -1,6 +1,6 @@
 from ows_refactored.land_and_vegetation.fc.band_fc_cfg import bands_fc_3
 from ows_refactored.land_and_vegetation.fc.style_fc_cfg import (
-    styles_fc_c3_masked, styles_fc_c3_unmasked)
+    style_fc_bs_c3, style_fc_c3_rgb, style_fc_gv_c3, style_fc_ngv_c3)
 from ows_refactored.ows_reslim_cfg import reslim_standard
 
 layer = {
@@ -50,9 +50,11 @@ For service status information, see https://status.dea.ga.gov.au""",
     ],
     "styling": {
         "default_style": "fc_rgb",
-        # FOR DEV ONLY - DO NOT PROMOTE UNMASKED STYLES TO PROD
-        "styles": styles_fc_c3_masked + styles_fc_c3_unmasked
-        # FOR PROD ONLY - DO NOT REMOVE UNMASKED STYLES FROM DEV
-        # "styles": styles_fc_c3_masked
+        "styles": [
+            style_fc_c3_rgb,
+            style_fc_bs_c3,
+            style_fc_gv_c3,
+            style_fc_ngv_c3,
+        ],
     },
 }
