713efc3b8cd6125351982d8cb0a6e01a819ce287
Jonathan Mettes <jmettes@gmail.com>
2022-11-11 15:59:54 +1100

    Update S2 C3 layer names to be consistent with others

--- prod/services/wms/ows_refactored/baseline_satellite_data/sentinel2/ows_ard_c3_cfg.py
+++ prod/services/wms/ows_refactored/baseline_satellite_data/sentinel2/ows_ard_c3_cfg.py
@@ -6,7 +6,7 @@ from ows_refactored.ows_reslim_cfg import reslim_for_sentinel2
 
 s2b_c3_layer = {
     "name": "ga_s2bm_ard_3",
-    "title": "DEA Surface Reflectance (Sentinel-2B MSI) Collection 3",
+    "title": "DEA Surface Reflectance (Sentinel-2B MSI,  Collection 3)",
     "abstract": """Sentinel-2 Multispectral Instrument - Nadir BRDF Adjusted Reflectance + Terrain Illumination Correction (Sentinel-2B MSI)
 
 This product has been corrected to account for variations caused by atmospheric properties, sun position and sensor view angle at time of image capture.
@@ -50,7 +50,7 @@ For service status information, see https://status.dea.ga.gov.au""",
 
 s2a_c3_layer = {
     "name": "ga_s2am_ard_3",
-    "title": "DEA Surface Reflectance (Sentinel-2A MSI) Collection 3",
+    "title": "DEA Surface Reflectance (Sentinel-2A MSI, Collection 3)",
     "abstract": """Sentinel-2 Multispectral Instrument - Nadir BRDF Adjusted Reflectance + Terrain Illumination Correction (Sentinel-2A MSI)
 
 This product takes Sentinel-2A imagery captured over the Australian continent and corrects for inconsistencies across land and coastal fringes. The result is accurate and standardised surface reflectance data, which is instrumental in identifying and quantifying environmental change.
@@ -93,7 +93,7 @@ For service status information, see https://status.dea.ga.gov.au""",
 }
 
 combined_layer = {
-    "title": "DEA Surface Reflectance (Sentinel-2 MSI) Collection 3",
+    "title": "DEA Surface Reflectance (Sentinel-2 MSI, Collection 3)",
     "name": "ga_s2m_ard_3",
     "abstract": """Sentinel-2 Multispectral Instrument - Nadir BRDF Adjusted Reflectance + Terrain Illumination Correction (Sentinel-2 MSI)
 
