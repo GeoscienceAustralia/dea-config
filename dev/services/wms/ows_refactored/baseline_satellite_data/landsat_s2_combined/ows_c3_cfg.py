9bd558d8f8aa2e0c432a01c81ba91bc8cfb2a76b
Paul Haesler <linkjuggler@gmail.com>
2023-03-20 15:22:22 +1100

    More flake8age.

--- dev/services/wms/ows_refactored/baseline_satellite_data/landsat_s2_combined/ows_c3_cfg.py
+++ dev/services/wms/ows_refactored/baseline_satellite_data/landsat_s2_combined/ows_c3_cfg.py
@@ -94,8 +94,8 @@ For service status information, see https://status.dea.ga.gov.au""",
         "function": "datacube_ows.ogc_utils.rolling_window_ndays",
         "pass_layer_cfg": True,
         "kwargs": {
-           "ndays": 6,
-        } 
+            "ndays": 6,
+        }
     },
     "bands": bands_ls_s2_shared,
     "resource_limits": reslim_for_sentinel2,
