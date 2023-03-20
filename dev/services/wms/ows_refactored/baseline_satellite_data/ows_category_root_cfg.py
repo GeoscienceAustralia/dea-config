2b8d30ba286cb211fd34b290ee32490e3170884d
Paul Haesler <linkjuggler@gmail.com>
2023-03-20 14:58:22 +1100

    Timeless mosaic layer of latest LS/S2 imagery.

--- dev/services/wms/ows_refactored/baseline_satellite_data/ows_category_root_cfg.py
+++ dev/services/wms/ows_refactored/baseline_satellite_data/ows_category_root_cfg.py
@@ -7,6 +7,10 @@ category_layers = {
             "type": "python",
         },
         {
+            "include": "ows_refactored.baseline_satellite_data.landsat_s2_combined.ows_c3_cfg.combined_mosaic_layer",
+            "type": "python",
+        },
+        {
             "include": "ows_refactored.baseline_satellite_data.landsat.ows_c3_cfg.combined_layer",
             "type": "python",
         },
