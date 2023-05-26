57cd6e5c77c3dff49ad886829a5f79e5e5115aca
erin telfer <erin.telfer@ga.gov.au>
2023-05-26 16:23:55 +1000

    add abstract text and generate legend

--- dev/services/wms/ows_refactored/hazards/burntarea/ows_ls_burncube_cfg.py
+++ dev/services/wms/ows_refactored/hazards/burntarea/ows_ls_burncube_cfg.py
@@ -8,7 +8,7 @@ bands_in_burncube = {
 
 style_combined_burncube = {
     "name": "combined_burncube",
-    "title": "Burncube Burnt Area",
+    "title": "BurnCube: Burnt Area",
     "abstract": "",
     "needed_bands": ["wofssevere", "wofsmoderate"],
     "index_expression": "2*wofssevere + wofsmoderate",
@@ -18,13 +18,13 @@ style_combined_burncube = {
         {"value": 2, "color": "#C0504D"},
     ],
     "legend": {
-        "url": "https://dea-public-data-dev.s3.ap-southeast-2.amazonaws.com/derivative/ga_s2_ba_provisional_3/dnbr_class_legend_edit.png",
+        "url": "https://dea-public-data-dev.s3-ap-southeast-2.amazonaws.com/projects/burn_cube/legend_combined.png",
     },
 }
 
 style_severe_burncube = {
     "name": "severe_burncube",
-    "title": "Burncube Severe",
+    "title": "BurnCube: Severe",
     "abstract": "burnt area",
     "needed_bands": ["wofssevere"],
     "index_function": {
@@ -49,7 +49,7 @@ style_severe_burncube = {
 
 style_moderate_burncube = {
     "name": "moderate_burncube",
-    "title": "Burncube Moderate",
+    "title": "BurnCube: Moderate",
     "abstract": "burnt area",
     "needed_bands": ["wofsmoderate"],
     "index_function": {
@@ -73,17 +73,30 @@ style_moderate_burncube = {
 }
 
 layers = {
-    "title": "DEA Burncube Burnt Area",
+    "title": "DEA BurnCube Historic Burnt Area",
     "abstract": "",
     "layers": [
         {
-            "title": "DEA Burncube Burnt Area (Landsat)",
+            "title": "DEA BurnCube Historic Burnt Area (Landsat)",
             "name": "ga_ls_nbart_bc_fyear_3_demo",
-            "abstract": """Burncube Burnt Area 30m 0.0.0 (Landsat, Collection 3)
-            ADD words about the product.
-            ADD DOI and collaboration.
-            ADD CMI https://cmi.ga.gov.au/data-products/dea/634/dea-mangrove-canopy-cover-landsat
+            "abstract": """BurnCube Historic Burnt Area, Calendar Year, 30m, 0.0.0 (Landsat, Collection 3)
+
+            Bushfires pose a serious and increasing threat to Australia. The detection and mapping of burns have many applications to support communities and ecosystems impacted by fire. Digital Earth Australia (DEA) offer a preliminary data product which was developed in collaboration with the Australian National University (ANU) to map historic burns with Landsat 8 data within continental Australia. 
+            
+            The BurnCube burnt area method is presented in (Renzullo et al. 2019) and analyses spectral anomalies. The method starts by using four years’ worth of satellite data to generate a Geomedian (Roberts, et al. 2017) which represents the average landscape over that time period. Next, the BurnCube burnt area method takes the following years’ worth of satellite data and detects deviations from the normal spectrum of each pixel (i.e. the difference of an area of land to the normal landscape over the last four years).
+            
+            BurnCube Historic Burnt Area is styled in three layers; “Moderate Burn”, “Severe Burn”, and “Combined Burn: Moderate and Severe”. Each layer contains identifies area that have shown the characteristic being burnt during that calendar year.
+            
+            This data is preliminary and provisional in nature and is still undergoing further development. These metrics should be used as a preliminary screening tool, and not an accurate identification of fire extent. These metrics should be used in combination with each other and can be used with other datasets to strengthen the agreement that the area has indeed been burnt. No decisions on life or property should be made based on this data.
+            
+            Renzullo, L & Tian, Siyuan & van Dijk, Albert & Rozas Larraondo, Pablo & Yebra, Marta & Yuan, F & Mueller, N. (2019). Burn extent and severity mapping by spectral anomaly detection in the Landsat data cube.
+            
+            Roberts, Dale & Mueller, Norman & McIntyre, Alexis. (2017). High-Dimensional Pixel Composites From Earth Observation Time Series. IEEE Transactions on Geoscience and Remote Sensing. PP. 1-11. 10.1109/TGRS.2017.2723896. 
+            
+            To do, add link to CMI when released, e.g. CMI https://cmi.ga.gov.au/data-products/dea/634/dea-mangrove-canopy-cover-landsat
+            
             For service status information, see https://status.dea.ga.gov.au""",
+
             "product_name": "ga_ls_nbart_bc_fyear_3_demo",
             "bands": bands_in_burncube,
             "resource_limits": reslim_standard,
