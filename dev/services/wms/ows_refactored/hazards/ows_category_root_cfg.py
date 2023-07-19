a4941469303c3b22673dbdbdd06a049cce422c98
Eloise <elly.birchall@gmail.com>
2023-07-19 03:34:23 +0000

    adding to hazards config and updating terria config

--- dev/services/wms/ows_refactored/hazards/ows_category_root_cfg.py
+++ dev/services/wms/ows_refactored/hazards/ows_category_root_cfg.py
@@ -10,6 +10,10 @@ category_layers = {
             "include": "ows_refactored.hazards.burntarea.ows_ls_burncube_cfg.layers",
             "type": "python",
         },
+                {
+            "include": "ows_refactored.hazards.burntarea.ows_ls_burncube_fy_cfg.layers",
+            "type": "python",
+        },
         {
             "include": "ows_refactored.hazards.burntarea.ows_s2_burncube_cyear_cfg.layers",
             "type": "python",
