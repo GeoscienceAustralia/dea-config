3212db11e3d144105b5877083e7adbb64fafd631
erin telfer <erin.telfer@ga.gov.au>
2023-07-24 15:30:11 +1000

    update ows hazard structure

---
+++ dev/services/wms/ows_refactored/hazards/nrt_burn/ows_category_root_cfg.py
@@ -0,0 +1,22 @@
+category_layers = {
+    "title": "Hazards",
+    "abstract": "",
+    "layers": [
+        {
+            "include": "ows_refactored.hazards.burntarea.ows_provisional_ba_cfg.layers",
+            "type": "python",
+        },
+        {
+            "include": "ows_refactored.hazards.burntarea.ows_ls_burncube_cfg.layers",
+            "type": "python",
+        },
+        {
+            "include": "ows_refactored.hazards.burntarea.ows_ls_burncube_fy_cfg.layers",
+            "type": "python",
+        },
+        {
+            "include": "ows_refactored.hazards.burntarea.ows_s2_burncube_cyear_cfg.layers",
+            "type": "python",
+        },
+    ]
+}
