c9ed19d3ecd1678efb380b93642c0771877df090
matt.e <matthew.ellis@ga.gov.au>
2023-07-31 16:03:25 +1000

    fixed indent

--- dev/services/wms/ows_refactored/hazards/historic_burn/ows_category_historic_cfg.py
+++ dev/services/wms/ows_refactored/hazards/historic_burn/ows_category_historic_cfg.py
@@ -14,7 +14,7 @@ layers = {
             "include": "ows_refactored.hazards.historic_burn.ows_s2_burncube_cyear_cfg.layers",
             "type": "python",
         },
-                {
+        {
             "include": "ows_refactored.hazards.historic_burn.ows_s2_burncube_fyear_cfg.layers",
             "type": "python",
         },
