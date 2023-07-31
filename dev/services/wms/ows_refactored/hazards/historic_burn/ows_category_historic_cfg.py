e473ac087805a437bfb704ffa5202dfc2f2778c8
Matt-dea <129345253+Matt-dea@users.noreply.github.com>
2023-07-31 16:08:32 +1000

    adding ows for burncube sentinel-2 fyear (#1203)
    * adding ows for burncube sentinel-2 fyear
    
    * Updated ows_category_historic_cfg to include bc s2 fyear
    
    * fixed indent

--- dev/services/wms/ows_refactored/hazards/historic_burn/ows_category_historic_cfg.py
+++ dev/services/wms/ows_refactored/hazards/historic_burn/ows_category_historic_cfg.py
@@ -14,5 +14,9 @@ layers = {
             "include": "ows_refactored.hazards.historic_burn.ows_s2_burncube_cyear_cfg.layers",
             "type": "python",
         },
+        {
+            "include": "ows_refactored.hazards.historic_burn.ows_s2_burncube_fyear_cfg.layers",
+            "type": "python",
+        },
     ]
 }
