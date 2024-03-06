f5826306afbf9dc7fd9e5be9cbd2483048ce005d
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2024-03-07 10:14:54 +1100

    Update documentation

--- dev/services/wms/ows_refactored/sea_ocean_coast/intertidal_c3/style_intertidal_cfg.py
+++ dev/services/wms/ows_refactored/sea_ocean_coast/intertidal_c3/style_intertidal_cfg.py
@@ -3,14 +3,25 @@ from datacube_ows.band_utils import scalable
 
 @scalable
 def elevation_adaptive(data, band, lot, hot, band_mapper=None):
+    """
+    Experimental adaptive elevation function, using pixel-level
+    tide metadata to calculate relative elevation for any 
+    given location. 
+
+    This implementation should be free of any tile-based
+    discontinuities in the resulting visualisation.
+
+    # TODO: Add hillshading
+    """
 
     # Calculate observed tide range (max - min)
     otr = data[hot] - data[lot]
 
-    # Calculate the distance between min and third_array value
+    # Calculate distance between elevation and minumum
+    # observed tide height
     distance_to_min = data[band] - data[lot]
 
-    # Calculate proportion
+    # Calculate proportion along observed tide range
     proportion_array = distance_to_min / otr
 
     return proportion_array
