4c07fcf774db770b566c26b021021291c755085c
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2024-03-07 14:07:47 +1100

    Add DEA Intertidal OWS layers (#1236)
    * Add starting point for DEA Intertidal OWS
    
    * Add additional styles, category root path
    
    * Updatr Flake8
    
    * Fix isort
    
    * Fix import
    
    * Try different multiline import format
    
    * Remove space
    
    * Update style name
    
    * Remove trailing comma
    
    * Add init
    
    * Update styles
    
    * Fix flake
    
    * More flake
    
    * Update documentation
    
    * Flake8
    
    * Use qualified name
    
    * Flake

---
+++ dev/services/wms/ows_refactored/sea_ocean_coast/intertidal_c3/ows_intertidal_cfg.py
@@ -0,0 +1,40 @@
+from ows_refactored.ows_reslim_cfg import reslim_standard
+from ows_refactored.sea_ocean_coast.intertidal_c3.style_intertidal_cfg import \
+    styles_intertidal_list
+
+bands_intertidal = {
+    "elevation": [],
+    "elevation_uncertainty": [],
+    "exposure": [],
+    "extents": [],
+    "ta_hat": [],
+    "ta_hot": [],
+    "ta_lat": [],
+    "ta_lot": [],
+    "ta_offset_high": [],
+    "ta_offset_low": [],
+    "ta_spread": [],
+}
+
+abstract_intertidal = """Geoscience Australia Sentinel-2 Landsat Intertidal Calendar Year Collection 3"""
+
+dea_intertidal_layer = {
+    "title": "DEA Intertidal (Sentinel-2, Landsat)",
+    "name": "ga_s2ls_intertidal_cyear_3",
+    "abstract": abstract_intertidal,
+    "product_name": "ga_s2ls_intertidal_cyear_3",
+    "bands": bands_intertidal,
+    "time_resolution": "summary",
+    "resource_limits": reslim_standard,
+    "native_crs": "EPSG:3577",
+    "native_resolution": [10, -10],
+    "image_processing": {
+        "extent_mask_func": "datacube_ows.ogc_utils.mask_by_val",
+        "always_fetch_bands": [],
+        "manual_merge": False,
+    },
+    "styling": {
+        "default_style": "intertidal_elevation",
+        "styles": styles_intertidal_list,
+    },
+}
---
+++ dev/services/wms/ows_refactored/sea_ocean_coast/intertidal_c3/style_intertidal_cfg.py
@@ -0,0 +1,211 @@
+legend_intertidal_percentage_by_20 = {
+    "begin": "0.0",
+    "end": "100",
+    "decimal_places": 1,
+    "ticks_every": "20",
+    "units": "%",
+    "tick_labels": {
+        "0": {"label": "0"},
+        "20": {"label": "20"},
+        "40": {"label": "40"},
+        "60": {"label": "60"},
+        "80": {"label": "80"},
+        "100": {"label": "100"},
+    },
+}
+
+style_intertidal_elevation = {
+    "name": "intertidal_elevation",
+    "title": "Elevation",
+    "abstract": "Intertidal elevation in metres above Mean Sea Level",
+    "index_function": {
+        "function": "datacube_ows.band_utils.single_band",
+        "mapped_bands": True,
+        "kwargs": {
+            "band": "elevation",
+        },
+    },
+    "include_in_feature_info": False,
+    "needed_bands": ["elevation"],
+    "mpl_ramp": "viridis",
+    "range": [-2.0, 1.0],
+    "legend": {
+        "begin": "-2.0",
+        "end": "1.0",
+        "ticks": ["-2.0", "-1.0", "0.0", "1.0"],
+        "units": "metres above Mean Sea Level",
+        "tick_labels": {
+            "1.0": {"prefix": ">"},
+            "-2.0": {"prefix": "<"},
+        },
+    },
+}
+
+style_intertidal_elevation_macro = {
+    "name": "intertidal_elevation_macro",
+    "title": "Elevation (macrotidal style)",
+    "abstract": "Intertidal elevation in metres above Mean Sea Level",
+    "index_function": {
+        "function": "datacube_ows.band_utils.single_band",
+        "mapped_bands": True,
+        "kwargs": {
+            "band": "elevation",
+        },
+    },
+    "include_in_feature_info": False,
+    "needed_bands": ["elevation"],
+    "mpl_ramp": "viridis",
+    "range": [-4.0, 2.0],
+    "legend": {
+        "begin": "-4.0",
+        "end": "2.0",
+        "ticks": ["-4.0", "-2.0", "0.0", "2.0"],
+        "units": "metres above Mean Sea Level",
+        "tick_labels": {
+            "2.0": {"prefix": ">"},
+            "-4.0": {"prefix": "<"},
+        },
+    },
+}
+
+style_intertidal_elevation_uncertainty = {
+    "name": "intertidal_elevation_uncertainty",
+    "title": "Elevation uncertainty",
+    "abstract": "Intertidal elevation uncertainty in metres",
+    "index_function": {
+        "function": "datacube_ows.band_utils.single_band",
+        "mapped_bands": True,
+        "kwargs": {
+            "band": "elevation_uncertainty",
+        },
+    },
+    "include_in_feature_info": False,
+    "needed_bands": ["elevation_uncertainty"],
+    "mpl_ramp": "inferno",
+    "range": [0.0, 1.0],
+    "legend": {
+        "begin": "0.0",
+        "end": "1.0",
+        "ticks": ["0.0", "0.5", "1.0"],
+        "units": "metres",
+        "tick_labels": {
+            "1.0": {"prefix": ">"},
+        },
+    },
+}
+
+style_intertidal_exposure = {
+    "name": "intertidal_exposure",
+    "title": "Exposure",
+    "abstract": "Intertidal exposure in percent of time exposed to air",
+    "index_function": {
+        "function": "datacube_ows.band_utils.single_band",
+        "mapped_bands": True,
+        "kwargs": {
+            "band": "exposure",
+        },
+    },
+    "include_in_feature_info": False,
+    "needed_bands": ["exposure"],
+    "color_ramp": [
+        {"value": 0, "color": '#2f0f3d'},
+        {'value': 10, 'color': '#4f1552'},
+        {'value': 20, 'color': '#72195f'},
+        {'value': 30, 'color': '#931f63'},
+        {'value': 40, 'color': '#b32e5e'},
+        {'value': 50, 'color': '#ce4356'},
+        {'value': 60, 'color': '#e26152'},
+        {'value': 70, 'color': '#ee845d'},
+        {'value': 80, 'color': '#f5a672'},
+        {'value': 90, 'color': '#faca8f'},
+        {'value': 100, 'color': '#fdedb0'}
+    ],
+    "legend": legend_intertidal_percentage_by_20,
+}
+
+style_intertidal_extents = {
+    "name": "intertidal_extents",
+    "title": "Extents",
+    "abstract": "Intertidal Extents classification",
+    "needed_bands": ["extents"],
+    "value_map": {
+        "extents": [
+            {
+                "title": "Dry",
+                "abstract": "",
+                "values": [0],
+                "color": "#dddddd",
+            },
+            {
+                "title": "Inland intermittent wet",
+                "abstract": "",
+                "values": [1],
+                "color": "#00a7c8",
+            },
+            {
+                "title": "Inland persistent wet",
+                "abstract": "",
+                "values": [2],
+                "color": "#00667a",
+            },
+            {
+                "title": "Tidally influenced wet",
+                "abstract": "",
+                "values": [3],
+                "color": "#2a5fa1",
+            },
+            {
+                "title": "Intertidal (low confidence)",
+                "abstract": "",
+                "values": [4],
+                "color": "#ffe8a1",
+            },
+            {
+                "title": "Intertidal (high confidence)",
+                "abstract": "",
+                "values": [5],
+                "color": "#ffc001",
+            },
+        ]
+    }
+}
+
+style_intertidal_elevation_adaptive = {
+    "name": "intertidal_elevation_adaptive",
+    "title": "Elevation",
+    "abstract": "Intertidal elevation in metres above Mean Sea Level",
+    "index_function": {
+        "function": "ows_refactored.sea_ocean_coast.intertidal_c3.utils_intertidal.elevation_adaptive",
+        "mapped_bands": True,
+        "kwargs": {
+            "band": "elevation",
+            "lot": "ta_lot",
+            "hot": "ta_hot",
+        },
+    },
+    "include_in_feature_info": False,
+    "needed_bands": ["elevation"],
+    "mpl_ramp": "viridis",
+    "range": [0.05, 0.7],
+    "legend": {
+        "begin": "0.0",
+        "end": "1.0",
+        "ticks": ["0.0", "0.5", "1.0"],
+        "units": "",
+        "tick_labels": {
+            "0.0": {"label": "Low"},
+            "0.5": {"label": ""},
+            "1.0": {"label": "High"},
+        },
+    },
+}
+
+# Create combined list that is imported and passed to the layer
+styles_intertidal_list = [
+    style_intertidal_elevation,
+    style_intertidal_elevation_macro,
+    style_intertidal_elevation_adaptive,
+    style_intertidal_elevation_uncertainty,
+    style_intertidal_exposure,
+    style_intertidal_extents,
+]
