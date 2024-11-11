260bed67146718443b65cf0f487e94643e2e40eb
James Miller <49048036+JM-GA@users.noreply.github.com>
2024-11-11 14:45:51 +1100

    Update ows_category_root_cfg.py

--- dev/services/wms/ows_refactored/land_and_vegetation/ows_category_root_cfg.py
+++ dev/services/wms/ows_refactored/land_and_vegetation/ows_category_root_cfg.py
@@ -9,7 +9,7 @@ category_layers = {
         {
             "include": "ows_refactored.land_and_vegetation.landcover.ows_c3_lc_cyear_cfg.layers",
             "type": "python",
-        },        
+        },
         {
             "include": "ows_refactored.land_and_vegetation.fc.ows_fc_cfg.layers",
             "type": "python",
