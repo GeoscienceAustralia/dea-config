88fb678c1f3106621b4b3a0b0971897e5e231037
Cate Kooymans <Cate.kooymans@ga.gov.au>
2025-05-13 05:50:53 +0000

    FMC combined layers

--- dev/services/wms/ows_refactored/hazards/ows_category_root_cfg.py
+++ dev/services/wms/ows_refactored/hazards/ows_category_root_cfg.py
@@ -19,11 +19,11 @@ category_layers = {
             "type": "python",
         },
         {
-            "include": "ows_refactored.hazards.dea_fmc.ows_fmc_combined_cfg.ga_s2_fmc",
+            "include": "ows_refactored.hazards.dea_fmc.ows_fmc_combined_cfg.ga_s2_fmc_layer",
             "type": "python",
         },
         {
-            "include": "ows_refactored.hazards.dea_fmc.ows_fmc_combined_cfg.ga_s2m_fmc_mosaic",
+            "include": "ows_refactored.hazards.dea_fmc.ows_fmc_combined_cfg.ga_s2m_fmc_mosaic_layer",
             "type": "python",
         },
         {
