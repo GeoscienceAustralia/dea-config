b4b2d0d44dc982f3603d88c0538c99431e837290
biancafichera <biancafichera00@gmail.com>
2025-06-03 07:30:05 +0000

    updated all files to include fmc s2c product

--- dev/services/wms/ows_refactored/hazards/dea_fmc/ows_fmc_cfg.py
+++ dev/services/wms/ows_refactored/hazards/dea_fmc/ows_fmc_cfg.py
@@ -108,3 +108,30 @@ s2b_layer = {
         "styles": [fmc_red, fmc_blue]
     }
 }
+
+s2c_layer = {
+    "title": "DEA Fuel Moisture Content (Sentinel 2c)",
+    "name": "ga_s2cm_fmc",
+    "abstract": """DEA Fuel Moisture Content (Sentinel 2c)""",
+    "product_name": "ga_s2cm_fmc",
+    "bands": bands_fmc,
+    "resource_limits": reslim_for_sentinel2,
+    "dynamic": True,
+    "native_crs": "EPSG:3577",
+    "native_resolution": [20, -20],
+    "image_processing": {
+        "extent_mask_func": "datacube_ows.ogc_utils.mask_by_val",
+        "always_fetch_bands": [],
+        "manual_merge": False
+    },
+    "flags": [{
+        "band": "land",
+        "product": "geodata_coast_100k",
+        "ignore_time": True,
+        "ignore_info_flags": []
+    }],
+    "styling": {
+        "default_style": "fmc_blue",
+        "styles": [fmc_red, fmc_blue]
+    }
+}
