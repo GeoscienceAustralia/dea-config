e6a89e2f1bbddd64c9c10b3430c4686203397219
Cate Kooymans <Cate.kooymans@ga.gov.au>
2025-08-04 06:36:19 +0000

    pretty feature info FMC

--- dev/services/wms/ows_refactored/hazards/dea_fmc/ows_fmc_cfg.py
+++ dev/services/wms/ows_refactored/hazards/dea_fmc/ows_fmc_cfg.py
@@ -79,13 +79,6 @@ s2a_layer = {
     "styling": {
         "default_style": "style_fmc",
         "styles": [style_fmc, style_fmc_old]
-    },
-    "feature_info": {
-        "include_custom": {
-            "description": {
-                "function": "ows_refactored.hazards.dea_fmc.fmc_feat_desc.class_labels",
-            }
-        }
     }
 }
 
