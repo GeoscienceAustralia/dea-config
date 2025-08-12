a4ebb8eeb15f9e44cbfa4da99c175bd3c7c51f90
Caitlin Adams <25995927+caitlinadams@users.noreply.github.com>
2025-08-13 09:23:34 +0930

    Add minimal OWS configuration for Sentinel-1 normalised radar backscatter

---
+++ dev/services/wms/ows_refactored/baseline_satellite_data/sentinel1/band_s1_nrb_cfg.py
@@ -0,0 +1,10 @@
+bands_sentinel1_nrb = {
+    "VV_gamma0": ["VV_gamma0", "vv_gamma0"],
+    "VH_gamma0": ["VH_gamma0", "vh_gamma0"],
+    "mask": ["mask"],
+    "number_of_looks": ["number_of_looks", "n_looks", "nlooks"],
+    "gamma0_to_beta0_ratio": ["gamma0_to_beta0_ratio"],
+    "gamma0_to_sigma0_ratio": ["gamma0_to_sigma0_ratio"],
+    "local_incidence_angle": ["local_incidence_angle", "lia", "LIA"],
+    "incidence_angle": ["incidence_angle", "ia", "IA"],
+}
---
+++ dev/services/wms/ows_refactored/baseline_satellite_data/sentinel1/style_s1_nrb_cfg.py
@@ -0,0 +1,11 @@
+style_s1_nrb_vv_gamma0 = {
+    "name": "vv_gamma0",
+    "title": "Linear backscatter VV gamma0",
+    "components": {
+        "red": {"VV_gamma0": 1},
+        "green": {"VV_gamma0": 1},
+        "blue": {"VV_gamma0": 1},
+    },
+}
+
+styles_s1_nrb_list = [style_s1_nrb_vv_gamma0]
