d2ef75d58c27734b1f69f82f253ea0d1f5df2016
Caitlin Adams <25995927+caitlinadams@users.noreply.github.com>
2025-08-13 10:30:11 +0930

    Add scale range for minimal style

--- dev/services/wms/ows_refactored/baseline_satellite_data/sentinel1/style_s1_nrb_cfg.py
+++ dev/services/wms/ows_refactored/baseline_satellite_data/sentinel1/style_s1_nrb_cfg.py
@@ -2,10 +2,11 @@ style_s1_nrb_vv_gamma0 = {
     "name": "vv_gamma0",
     "title": "Linear backscatter VV gamma0",
     "components": {
-        "red": {"VV_gamma0": 1},
-        "green": {"VV_gamma0": 1},
-        "blue": {"VV_gamma0": 1},
+        "red": {"VV_gamma0": 1.0},
+        "green": {"VV_gamma0": 1.0},
+        "blue": {"VV_gamma0": 1.0},
     },
+    "scale_range": (0.02, 1.9),
 }
 
 styles_s1_nrb_list = [style_s1_nrb_vv_gamma0]
