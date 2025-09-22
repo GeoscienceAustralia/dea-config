f82d9480bd247526b590d596c341458d300dc896
Caitlin Adams <25995927+caitlinadams@users.noreply.github.com>
2025-09-22 15:19:13 +0930

    Fix typo on category layer import for Sentinel-1

--- dev/services/wms/ows_refactored/baseline_satellite_data/sentinel1/ows_s1_cfg.py
+++ dev/services/wms/ows_refactored/baseline_satellite_data/sentinel1/ows_s1_cfg.py
@@ -3,7 +3,7 @@ category_layers = {
     "abstract": "",
     "layers": [
         {
-            "include": "ows_refactored.baseline_satellite_data.sentinel1.ows_s1_nrb_config.iw_vv_vh_0_layer",
+            "include": "ows_refactored.baseline_satellite_data.sentinel1.ows_s1_nrb_cfg.iw_vv_vh_0_layer",
             "type": "python",
         },
     ],
