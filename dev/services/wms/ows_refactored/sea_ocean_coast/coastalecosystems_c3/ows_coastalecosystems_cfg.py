a570b9adcea245da0f30d058f0cb33cc5beabb0c
Vanessa Newey <vnewey@users.noreply.github.com>
2025-11-19 10:13:45 +1100

    Update abstract and title for coastal ecosystems config

--- dev/services/wms/ows_refactored/sea_ocean_coast/coastalecosystems_c3/ows_coastalecosystems_cfg.py
+++ dev/services/wms/ows_refactored/sea_ocean_coast/coastalecosystems_c3/ows_coastalecosystems_cfg.py
@@ -11,16 +11,16 @@ bands_coastalecosystems = {
     "qa_count_clear": [],
 }
 
-abstract_coastalecosystems = """Geoscience Australia Sentinel-2 Coastal Ecosystem Map Calendar Year Collection 3
+abstract_coastalecosystems = """Geoscience Australia Sentinel-2 Coastal Ecosystems Calendar Year Collection 3
 
-The Digital Earth Australia Coastal Ecosystem Map product suite includes a categorical classification of four coastal ecosystems, supported by probability layers for individual ecosystems and selected quality assurance layers to support product interpretation, for the years 2021 and 2022 and 10 m spatial resolution.
+The Digital Earth Australia Coastal Ecosystems product suite includes a categorical classification of four coastal ecosystems, supported by probability layers for individual ecosystems and selected quality assurance layers to support product interpretation, for the years 2021 and 2022 and 10 m spatial resolution.
 
 Product documentation: https://knowledge.dea.ga.gov.au/data/product/dea-coastalecosystems/
 
 Service status information: https://status.dea.ga.gov.au"""
 
 dea_coastalecosystems_layer = {
-    "title": "DEA Coastal Ecosystem Map (Sentinel-2)",
+    "title": "DEA Coastal Ecosystems (Sentinel-2)",
     "name": "ga_s2_coastalecosystems_cyear_3_v1",
     "abstract": abstract_coastalecosystems,
     "product_name": "ga_s2_coastalecosystems_cyear_3_v1",
