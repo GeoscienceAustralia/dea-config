ffbc87bc1c247a147f0cfae38a1ffb17c5df438a
Vanessa Newey <vnewey@users.noreply.github.com>
2025-11-18 07:52:35 +0000

    Added ga_s2_coastalecosystems_cyear_3_v1

--- dev/services/wms/ows_refactored/sea_ocean_coast/ows_category_root_cfg.py
+++ dev/services/wms/ows_refactored/sea_ocean_coast/ows_category_root_cfg.py
@@ -25,6 +25,17 @@ category_layers = {
             ]
         },
         {
+            "title": "DEA Coastal Ecosystem Map",
+            "abstract": "",
+            "layers": [
+                {
+                    # DEA Coastal Ecosystem Map Collection 3
+                    "include": "ows_refactored.sea_ocean_coast.coastalecosystems_c3.ows_coastalecosystems_cfg.dea_coastalecosystems_layer",
+                    "type": "python",
+                },
+            ]
+        },
+        {
             "title": "Other",
             "abstract": "",
             "layers": [
