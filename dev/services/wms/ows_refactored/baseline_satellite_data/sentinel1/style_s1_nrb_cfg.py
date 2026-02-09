ea96425ad70230185746d8546a6c468de4914152
arcisad <11005528+arcisad@users.noreply.github.com>
2026-02-09 05:05:37 +0000

    Add S1 nrb vv-vh styles

--- dev/services/wms/ows_refactored/baseline_satellite_data/sentinel1/style_s1_nrb_cfg.py
+++ dev/services/wms/ows_refactored/baseline_satellite_data/sentinel1/style_s1_nrb_cfg.py
@@ -22,6 +22,67 @@ style_s1_nrb_VV = {
     "scale_range": [0.02, 0.4],
 }
 
+## Scale range is p5 and p90 of samples ascorss Australia, calculated in s1_stats.py. 
+# The scale range is set to these values to enhance the contrast of the images and make it easier to distinguish different features in the data. 
+# The scale range can be adjusted based on the specific use case and the desired level of detail in the images.
+style_s1_nrb_false_colour_linear = {
+    "name": "vv_vh_false_colour_linear",
+    "title": "VV+VH False Colour",
+    "abstract": "VV+VH False Colour",
+    "additional_bands": ["VV_gamma0", "VH_gamma0"],
+    "components": {
+        "red": {
+            "VV_gamma0": 1.0,
+            "scale_range": [0.00978187, 0.12744568],
+        },
+        "green": {
+            "VH_gamma0": 1.0,
+            "scale_range": [0.00096179, 0.03024042],
+        },
+        "blue": {
+            "function": "datacube_ows.band_utils.band_quotient",
+            "mapped_bands": True,
+            "kwargs": {
+                "band1": "VH_gamma0",
+                "band2": "VV_gamma0",
+                "scale_from": [0.01472214, 1.10262954],
+            },
+        },
+    },
+}
+
+# scale range is p5 and p90 of samples across Australia.
+style_s1_nrb_false_colour_db = {
+    "name": "vv_vh_false_colour_db",
+    "title": "VV+VH False Colour (dB)",
+    "abstract": "VV+VH False Colour (dB)",
+    "additional_bands": ["VV_gamma0", "VH_gamma0"],
+    "components": {
+        "red": {
+            "function": "s1_style_functions.db",
+            "kwargs": {
+                "band": "VV_gamma0",
+                "scale_from": (-20.09524918, -8.94672203),
+            },
+        },
+        "green": {
+            "function": "s1_style_functions.db",
+            "kwargs": {
+                "band": "VH_gamma0",
+                "scale_from": (-30.15318871, -15.19352818),
+            },
+        },
+        "blue": {
+            "function": "s1_style_functions.db_difference",
+            "kwargs": {
+                "band1": "VH_gamma0",
+                "band2": "VV_gamma0",
+                "scale_from": (-18.30269623, 0.42520905),
+            },
+        },
+    },
+}
+
 style_s1_nrb_mask = {
     "name": "mask",
     "title": "Shadow Layover Mask",
@@ -35,4 +96,9 @@ style_s1_nrb_mask = {
 }
 
 
-styles_s1_nrb_vvvh_list = [style_s1_nrb_VV, style_s1_nrb_mask]
+styles_s1_nrb_vvvh_list = [
+    style_s1_nrb_VV,
+    style_s1_nrb_mask,
+    style_s1_nrb_false_colour_linear,
+    style_s1_nrb_false_colour_db,
+]
