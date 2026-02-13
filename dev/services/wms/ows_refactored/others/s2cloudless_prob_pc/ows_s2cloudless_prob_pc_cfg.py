9df161572fa173a70b8b9713a4a4a19c93fe54a8
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2026-02-13 14:56:11 +1100

    Fix isort

--- dev/services/wms/ows_refactored/others/s2cloudless_prob_pc/ows_s2cloudless_prob_pc_cfg.py
+++ dev/services/wms/ows_refactored/others/s2cloudless_prob_pc/ows_s2cloudless_prob_pc_cfg.py
@@ -1,6 +1,7 @@
+from ows_refactored.others.s2cloudless_prob_pc.style_s2cloudless_prob_pc_cfg import (
+    styles_s2cloudless_prob_pc_list,
+)
 from ows_refactored.ows_reslim_cfg import reslim_standard
-from ows_refactored.others.s2cloudless_prob_pc.style_s2cloudless_prob_pc_cfg import \
-    styles_s2cloudless_prob_pc_list
 
 bands_s2cloudless_prob_pc = {
     "oa_s2cloudless_prob_pc_5": [],
