5424f0088ddcea0e55c5b128532914ce40f30a95
Vanessa Newey <vnewey@users.noreply.github.com>
2026-04-14 13:42:27 +1000

    Refactor intertidal configuration for WMS service

--- dev/services/wms/ows_refactored/sea_ocean_coast/intertidal_c3/ows_intertidal_cfg.py
+++ dev/services/wms/ows_refactored/sea_ocean_coast/intertidal_c3/ows_intertidal_cfg.py
@@ -1,4 +1,4 @@
-from ows_refactored.ows_reslim_cfg import reslim_standard
+from ows_refactored.ows_reslim_cfg import reslim_wms_unlimited
 from ows_refactored.sea_ocean_coast.intertidal_c3.style_intertidal_cfg import \
     styles_intertidal_list
 
